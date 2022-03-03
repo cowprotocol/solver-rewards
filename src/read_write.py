09c7f7f6ab0ad70d58f45c3b0cf4f8078323c26b
Ben Smith <bh2smith@gmail.com>
2022-03-03 14:09:28 +0100

    add comment to csv writer

--- src/read_write.py
+++ src/read_write.py
@@ -28,8 +28,10 @@ def write_to_csv(data_list: list, outfile: File):
     headers = [f.name for f in fields(data_list[0])]
     data_tuple = [astuple(x) for x in data_list]
 
+    # Creates out path if it doesn't exist.
     if not os.path.exists(outfile.path):
         os.makedirs(outfile.path)
+
     with open(outfile.filename(), 'w', encoding='utf-8') as out_file:
         dict_writer = csv.DictWriter(out_file, headers, lineterminator='\n')
         dict_writer.writeheader()
