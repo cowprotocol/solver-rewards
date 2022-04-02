fabf8393388afc3260086da594570647461746c4
Ben Smith <bh2smith@gmail.com>
2022-04-02 18:32:26 +0200

    black fmt

--- src/fetch/period_slippage.py
+++ src/fetch/period_slippage.py
@@ -127,8 +127,8 @@ class SplitSlippages:
 
 
 def get_period_slippage(
-        dune: DuneAPI,
-        period: AccountingPeriod,
+    dune: DuneAPI,
+    period: AccountingPeriod,
 ) -> SplitSlippages:
     """
     Executes & Fetches results of slippage query per solver for specified accounting period.
