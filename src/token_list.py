a842b7f9627444158bff9fcc2dfccd3c8748baac
Ben Smith <bh2smith@gmail.com>
2022-06-14 15:08:03 +0200

    update allowed token list url

--- src/token_list.py
+++ src/token_list.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 # pylint: disable=line-too-long
-ALLOWED_TOKEN_LIST_URL = "https://raw.githubusercontent.com/gnosis/cow-dex-solver/main/data/token_list_for_buffer_trading.json"
+ALLOWED_TOKEN_LIST_URL = "https://token-list.cow.eth.link/"
 
 
 def parse_token_list(token_list_json: str) -> list[str]:
