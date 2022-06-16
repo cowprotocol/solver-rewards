3098b001d0990b30c29dd4d121e22bfb386ee1ac
Ben Smith <bh2smith@gmail.com>
2022-06-16 12:32:22 +0200

    eliminate this ridiculous test on unreliable link

--- tests/unit/test_update_token_list.py
+++ tests/unit/test_update_token_list.py
@@ -2,16 +2,10 @@ import unittest
 
 from duneapi.api import DuneAPI
 
-from src.token_list import fetch_trusted_tokens
 from src.update.token_list import update_token_list
 
 
 class TestTokenList(unittest.TestCase):
-    def test_token_list_update(self):
-        dune = DuneAPI.new_from_environment()
-        fetched_tokens = fetch_trusted_tokens()
-        dune_list = update_token_list(dune, fetched_tokens)
-        self.assertEqual(len(dune_list), len(fetched_tokens))
 
     def test_empty_token_list_update(self):
         dune = DuneAPI("user", "password")
