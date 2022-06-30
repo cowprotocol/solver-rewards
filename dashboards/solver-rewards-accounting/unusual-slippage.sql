5e619254394996e90b5de832a4f8c94c69bb4002
Benjamin Smith <bh2smith@users.noreply.github.com>
2022-06-30 07:46:24 +0200

    Modify Unusual Slippage Condition (#65)

--- dashboards/solver-rewards-accounting/unusual-slippage.sql
+++ dashboards/solver-rewards-accounting/unusual-slippage.sql
@@ -8,6 +8,10 @@ select
 from results_per_tx rpt
 join gnosis_protocol_v2."batches" b
     on rpt.tx_hash = b.tx_hash
-where abs(usd_value) > '{{SignificantSlippageValue}}'
-and 100.0 * abs(usd_value) / batch_value > '{{RelativeSlippageTolerance}}'
+where (
+    abs(usd_value) > '{{MinAbsoluteSlippageTolerance}}'
+    and
+    100.0 * abs(usd_value) / batch_value > '{{RelativeSlippageTolerance}}'
+) or
+    abs(usd_value) > '{{SignificantSlippageValue}}'
 order by relative_slippage
