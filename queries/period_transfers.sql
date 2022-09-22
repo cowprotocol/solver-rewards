1631844e664e4744b5537e1a1828d4f8e3029097
Ben Smith <bh2smith@gmail.com>
2022-09-22 11:00:22 +0200

    exclude test solvers from payout

--- queries/period_transfers.sql
+++ queries/period_transfers.sql
@@ -1,13 +1,17 @@
 with
 -- For a configurable permanent version visit: https://dune.xyz/queries/469110
 relevant_batch_info as (
+    -- This subquery can be played with here: https://dune.com/queries/1297092
     select concat('0x', encode(solver_address, 'hex')) as solver,
            sum(gas_price_gwei * gas_used) * 10 ^ 9     as eth_spent,
            count(*) * '{{PerBatchReward}}'             as batch_reward,
            sum(num_trades) * '{{PerTradeReward}}'      as trade_reward
     from gnosis_protocol_v2.batches
+    join gnosis_protocol_v2.view_solvers
+        on solver_address = address
     where block_time >= '{{StartTime}}'
       and block_time < '{{EndTime}}'
+      and environment not in ('services', 'test')
     group by solver
 )
 
