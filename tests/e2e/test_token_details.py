f44162c3e666a19e2f329ef1542a737d5a0bc522
Benjamin Smith <bh2smith@users.noreply.github.com>
2022-10-18 13:35:28 +0200

    update e2e tests and fix dependency (#98)

--- tests/e2e/test_token_details.py
+++ tests/e2e/test_token_details.py
@@ -6,21 +6,16 @@ from src.utils.token_details import get_token_decimals
 
 
 class TestTokenDecimals(unittest.TestCase):
-    def setUp(self) -> None:
-        self.cow = "0xDEf1CA1fb7FBcDC777520aa7f396b4E015F497aB"
-        self.usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-
     def test_token_decimals(self):
-        self.assertEqual(get_token_decimals(self.cow), 18)
-        self.assertEqual(get_token_decimals(self.usdc), 6)
-
-        self.assertEqual(get_token_decimals(duneapi.types.Address(self.usdc)), 6)
-        self.assertEqual(get_token_decimals(duneapi.types.Address(self.cow)), 18)
+        # Goerli doesn't seem to have tokens with anything other than 18 decimals.
+        cow = "0x3430d04E42a722c5Ae52C5Bffbf1F230C2677600"
+        self.assertEqual(get_token_decimals(cow), 18)
+        self.assertEqual(get_token_decimals(duneapi.types.Address(cow)), 18)
 
     def test_token_decimals_cache(self):
-        NEW_TOKEN = "0x10245515d35BC525e3C0977412322BFF32382EF1"
+        new_token = "0x10245515d35BC525e3C0977412322BFF32382EF1"
         with self.assertLogs("src.utils.token_details", level="INFO"):
-            get_token_decimals(NEW_TOKEN)
+            get_token_decimals(new_token)
 
         with self.assertNoLogs("src.utils.token_details", level="INFO"):
             get_token_decimals(new_token)
