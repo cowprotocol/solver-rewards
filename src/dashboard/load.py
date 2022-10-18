b0604ce5a4a22e7986051f7902d471a2e12527cf
Ben Smith <bh2smith@gmail.com>
2022-10-18 20:39:34 +0200

    revert unnecessary diff

--- src/dashboard/load.py
+++ src/dashboard/load.py
@@ -8,4 +8,4 @@ from src.dashboard.common import arg_parse
 
 if __name__ == "__main__":
     dune_connection, args = arg_parse(description="Load Dashboard")
-    dashboard = DuneDashboard.from_dune(dune_connection, args.slug)
+    dashboard = DuneDashboard.from_dune(dune_connection, args.dashboard_slug)
