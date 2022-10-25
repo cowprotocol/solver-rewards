d7e45462d577cdb382b914b7c527526b28f35b86
Ben Smith <bh2smith@gmail.com>
2022-10-25 09:59:09 +0200

    add the last test

--- tests/unit/test_reward_aggregation.py
+++ tests/unit/test_reward_aggregation.py
@@ -16,24 +16,51 @@ class MyTestCase(unittest.TestCase):
     cf: https://github.com/cowprotocol/solver-rewards/pull/107#issuecomment-1288566854
     """
 
-    # TODO - add this: 0x82318dd23592f7ccba72fcad43c452c4c426d9e02c7cf3b1f9e7823a0c9a9fc0
     def test_aggregate_orderbook_rewards(self):
-        # Tx 0x001 is 0x72e4c54e9c9dc2ee2a09dd242bf80abc39d122af0813ff4d570d3ce04eea8468
-        # Tx 0x002 is 0x43bfe76d590966c7539f1ea0bb7989edc1289f989eaf8d84589c3508c5066c2c
-        # Tx 0x003 is 0x6b6181e95ae837376dd15adbe7801bffffee639dbc8f18b918ace9645a5c1be2
-        solvers = ["0x1", "0x1", "0x2", "0x2", "0x3", "0x1", "0x2", "0x3"]
+        solvers = [
+            "0x1",
+            "0x1",
+            "0x2",
+            "0x2",
+            "0x3",
+            "0x1",
+            "0x2",
+            "0x3",
+            "0x4",
+            "0x4",
+            "0x4",
+        ]
         tx_hashes = [
+            # Tx 0x001 is 0x72e4c54e9c9dc2ee2a09dd242bf80abc39d122af0813ff4d570d3ce04eea8468
             "0x001",
             "0x001",
+            # Tx 0x002 is 0x43bfe76d590966c7539f1ea0bb7989edc1289f989eaf8d84589c3508c5066c2c
             "0x002",
             "0x002",
+            # Tx 0x003 is 0x6b6181e95ae837376dd15adbe7801bffffee639dbc8f18b918ace9645a5c1be2
             "0x003",
             "0x004",
             "0x005",
             "0x006",
+            # Tx 0x007 0x82318dd23592f7ccba72fcad43c452c4c426d9e02c7cf3b1f9e7823a0c9a9fc0
+            "0x007",
+            "0x007",
+            "0x007",
         ]
-        amounts = [39, 0, 40, 0, 41, 50, 60, 70]
-        safe_liquidity = [None, True, None, False, None, None, None, None]
+        amounts = [39, 0, 40, 0, 41, 50, 60, 70, 40, 50, 0]
+        safe_liquidity = [
+            None,
+            True,
+            None,
+            False,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            True,
+        ]
         orderbook_rewards = pd.DataFrame(
             {
                 "solver": solvers,
@@ -43,20 +70,24 @@ class MyTestCase(unittest.TestCase):
             }
         )
         results = aggregate_orderbook_rewards(
-            orderbook_rewards, risk_free_transactions={"0x001", "0x002", "0x003"}
+            orderbook_rewards,
+            risk_free_transactions={"0x001", "0x002", "0x003", "0x007"},
         )
         expected = pd.DataFrame(
             {
-                "receiver": ["0x1", "0x2", "0x3"],
-                "num_trades": [3, 3, 2],
-                "amount": [to_wei(87), to_wei(100), to_wei(107)],
+                "receiver": ["0x1", "0x2", "0x3", "0x4"],
+                "num_trades": [3, 3, 2, 3],
+                "amount": [to_wei(87), to_wei(100), to_wei(107), to_wei(74)],
                 "token_address": [
+                    "0xDEf1CA1fb7FBcDC777520aa7f396b4E015F497aB",
                     "0xDEf1CA1fb7FBcDC777520aa7f396b4E015F497aB",
                     "0xDEf1CA1fb7FBcDC777520aa7f396b4E015F497aB",
                     "0xDEf1CA1fb7FBcDC777520aa7f396b4E015F497aB",
                 ],
             }
         )
+        print(expected)
+        print(results)
         self.assertIsNone(pd.testing.assert_frame_equal(expected, results))
 
     def test_map_reward(self):
