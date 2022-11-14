01e9373b9055eaef656b82c039596a8bd618e967
Ben Smith <bh2smith@gmail.com>
2022-11-14 11:54:06 +0100

    optimizations

--- queries/dune_v2/period_slippage.sql
+++ queries/dune_v2/period_slippage.sql
@@ -1,12 +1,11 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
 with
--- V2 Query: https://dune.com/queries/1570561
--- Benchmark 8 Minutes for 1 Day without pro account.
 filtered_trades as (
     select t.block_time,
            t.tx_hash,
            dex_swaps,
            num_trades,
-        --   solver_name,
            solver_address,
            trader                                              as trader_in,
            receiver                                            as trader_out,
@@ -14,11 +13,12 @@ filtered_trades as (
            buy_token_address                                   as buy_token,
            atoms_sold,
            atoms_bought,
-           lower('0x9008D19f58AAbD9eD0D60971565AA8510560ab41') as contract_address
+           '0x9008d19f58aabd9ed0d60971565aa8510560ab41' as contract_address
     from cow_protocol_ethereum.trades t
          join cow_protocol_ethereum.batches b
             on t.tx_hash = b.tx_hash
     where b.block_time between '{{StartTime}}' and '{{EndTime}}'
+    and t.block_time between '{{StartTime}}' and '{{EndTime}}'
     and case
             when '{{TxHash}}' = '0x' then true
             else lower('{{TxHash}}') = t.tx_hash
@@ -40,7 +40,6 @@ user_in as (
           dex_swaps,
           num_trades,
           solver_address,
-        --   solver_name,
           trader_in        as sender,
           contract_address as receiver,
           sell_token       as token,
@@ -54,7 +53,6 @@ user_out as (
           dex_swaps,
           num_trades,
           solver_address,
-        --   solver_name,
           contract_address as sender,
           trader_out       as receiver,
           buy_token        as token,
@@ -68,15 +66,14 @@ other_transfers as (
           dex_swaps,
           num_trades,
           solver_address,
-        --   solver_name,
           from               as sender,
           to                 as receiver,
           t.contract_address as token,
           value              as amount_wei,
           case
-              when to = lower('0x9008D19f58AAbD9eD0D60971565AA8510560ab41')
+              when to = '0x9008d19f58aabd9ed0d60971565aa8510560ab41'
                   then 'IN_AMM'
-              when from = lower('0x9008D19f58AAbD9eD0D60971565AA8510560ab41')
+              when from = '0x9008d19f58aabd9ed0d60971565aa8510560ab41'
                   then 'OUT_AMM'
               end            as transfer_type
     from erc20_ethereum.evt_Transfer t
@@ -85,7 +82,7 @@ other_transfers as (
              inner join batchwise_traders bt
                 on evt_tx_hash = bt.tx_hash
     where b.block_time between '{{StartTime}}' and '{{EndTime}}'
-      and lower('0x9008D19f58AAbD9eD0D60971565AA8510560ab41') in (to, from)
+      and '0x9008d19f58aabd9ed0d60971565aa8510560ab41' in (to, from)
       and not array_contains(traders_in, from)
       and not array_contains(traders_out, to)
       and case
@@ -104,7 +101,7 @@ batch_transfers as (
 -- whose transfer function doesn't align with the emitted transfer event.
 exluded_batches as (
     select tx_hash from filtered_trades
-    where lower('0xf5d669627376ebd411e34b98f19c868c8aba5ada') in (buy_token, sell_token)
+    where '0xf5d669627376ebd411e34b98f19c868c8aba5ada' in (buy_token, sell_token)
 ),
 incoming_and_outgoing as (
     SELECT
@@ -112,22 +109,20 @@ incoming_and_outgoing as (
         tx_hash,
         dex_swaps,
         solver_address,
-        -- solver_name,
         case
             when t.symbol = 'ETH' then 'WETH'
             when t.symbol is not null then t.symbol
             else token
         end                                     as symbol,
           case
-              when token = lower('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-                  then lower('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+              when token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+                  then '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
               else token
               end                                     as token,
           case
-              when receiver =
-                    lower('0x9008D19f58AAbD9eD0D60971565AA8510560ab41')
+              when receiver = '0x9008d19f58aabd9ed0d60971565aa8510560ab41'
                   then amount_wei
-              when sender = lower('0x9008D19f58AAbD9eD0D60971565AA8510560ab41')
+              when sender = '0x9008d19f58aabd9ed0d60971565aa8510560ab41'
                   then -1 * amount_wei
               end                                     as amount,
           transfer_type
@@ -148,35 +143,34 @@ incoming_and_outgoing as (
 
 -- Clearing Prices query here: https://dune.com/queries/1571457
 pre_clearing_prices as (
-    select call_tx_hash as tx_hash,
-            i, j,
-          price,
-          token
+    select
+        call_tx_hash as tx_hash,
+        price,
+        token
     from gnosis_protocol_v2_ethereum.GPv2Settlement_call_settle
         lateral view posexplode(clearingPrices) as i, price
         lateral view posexplode(tokens) as j, token
-    where call_success = true
+    where call_block_time between '{{StartTime}}' and '{{EndTime}}'
+      and call_success = true
       and i = j
-      and call_block_time between '{{StartTime}}' and '{{EndTime}}'
-    order by call_block_number desc
 ),
 clearing_prices as (
-    select tx_hash,
-          case
-              when token = lower('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-                  then lower('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
-              else token
-              end as token,
-          avg(price) as clearing_price
+    select
+        tx_hash,
+        case
+            when token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+                then '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+            else token
+        end as token,
+        avg(price) as clearing_price
     from pre_clearing_prices
-    group by 1,2
+    group by tx_hash, token
 ),
 potential_buffer_trades as (
     select block_time,
           tx_hash,
           dex_swaps,
           solver_address,
-        --   solver_name,
           symbol,
           token,
           sum(amount) as amount
@@ -184,7 +178,6 @@ potential_buffer_trades as (
     group by tx_hash,
              dex_swaps,
              solver_address,
-            --  solver_name,
              symbol,
              token,
              block_time
@@ -205,7 +198,8 @@ valued_potential_buffered_trades as (
         on t.tx_hash = cp.tx_hash
     and t.token = cp.token
         left outer join prices.usd pusd
-            on pusd.contract_address = t.token
+            on pusd.minute between '{{StartTime}}' and '{{EndTime}}'
+            and pusd.contract_address = t.token
             and blockchain = 'ethereum'
             and date_trunc('minute', block_time) = pusd.minute
 ),
@@ -235,7 +229,6 @@ buffer_trades as (
     Select a.block_time as block_time,
           a.tx_hash,
           a.solver_address,
-        --   a.solver_name,
           a.symbol,
           a.token       as token_from,
           b.token       as token_to,
@@ -307,7 +300,6 @@ incoming_and_outgoing_with_buffer_trades as (
     select block_time,
           tx_hash,
           solver_address,
-        --   solver_name,
           symbol,
           token,
           amount,
@@ -317,7 +309,6 @@ incoming_and_outgoing_with_buffer_trades as (
     select block_time,
           tx_hash,
           solver_address,
-        --   solver_name,
           symbol,
           token_from as token,
           amount_from as amount,
@@ -327,7 +318,6 @@ incoming_and_outgoing_with_buffer_trades as (
 final_token_balance_sheet as (
     select
         solver_address,
-        -- solver_name,
         sum(amount) token_imbalance_wei,
         symbol,
         token,
@@ -339,7 +329,6 @@ final_token_balance_sheet as (
         symbol,
         token,
         solver_address,
-        -- solver_name,
         tx_hash,
         block_time
     having
@@ -396,7 +385,8 @@ intrinsic_prices as (
             date_trunc('hour', block_time) as hour,
             usd_value / units_bought as price
         FROM cow_protocol_ethereum.trades
-        WHERE units_bought > 0
+        WHERE block_time between '{{StartTime}}' and '{{EndTime}}'
+        AND units_bought > 0
     UNION
         select
             sell_token_address as contract_address,
@@ -404,7 +394,8 @@ intrinsic_prices as (
             date_trunc('hour', block_time) as hour,
             usd_value / units_sold as price
         FROM cow_protocol_ethereum.trades
-        WHERE units_sold > 0
+        WHERE block_time between '{{StartTime}}' and '{{EndTime}}'
+        AND units_sold > 0
     ) as combined
     GROUP BY hour, contract_address, decimals
 ),
@@ -441,7 +432,7 @@ eth_prices as (
         avg(price) as eth_price
     from prices.usd
     where blockchain = 'ethereum'
-    and contract_address = lower('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+    and contract_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
     and minute between '{{StartTime}}' and '{{EndTime}}'
     group by date_trunc('hour', minute)
 ),
@@ -449,7 +440,6 @@ results_per_tx as (
     select
         ftbs.hour,
         solver_address,
-        -- solver_name,
         sum(token_imbalance_wei * price / pow(10, p.decimals)) as usd_value,
         sum(token_imbalance_wei * price / pow(10, p.decimals) / eth_price) * pow(10, 18) as eth_slippage_wei,
         tx_hash
@@ -463,7 +453,6 @@ results_per_tx as (
     group by
         ftbs.hour,
         solver_address,
-        -- solver_name,
         tx_hash
     having
         bool_and(price is not null)
