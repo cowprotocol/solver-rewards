a1b542a669cfa1f831b4d6ce563954ba1507ba65
Benjamin Smith <bh2smith@users.noreply.github.com>
2022-12-13 13:35:24 +0100

    Remove Safe Liquidity (#157)
    This concept of safe liquidity is no longer required (cf here in slack). Here we implement the removal of this field from the order book query and trickle down through all the code that depended on it.

--- queries/user_generated/order_rewards_template.sql
+++ queries/user_generated/order_rewards_template.sql
@@ -2,8 +2,7 @@ select order_uid::bytea,
        solver::bytea,
        tx_hash::bytea,
        surplus_fee::numeric,
-       amount::numeric,
-       safe_liquidity::bool
+       amount::numeric
 from (VALUES
 {{Values}}
-) as _ (order_uid, solver, tx_hash, surplus_fee, amount, safe_liquidity)
+) as _ (order_uid, solver, tx_hash, surplus_fee, amount)
