054715edb99e7114261774c6adf81fa8f89ead5e
Benjamin Smith <bh2smith@users.noreply.github.com>
2022-12-21 12:54:54 +0100

    use batches table in all V2 queryes that involve Settlement Event (#164)

--- queries/dune_v2/risk_free_batches.sql
+++ queries/dune_v2/risk_free_batches.sql
@@ -1,4 +1,3 @@
--- V2 Query: https://dune.com/queries/1541507
 with
 interactions as (
     select
@@ -18,10 +17,10 @@ interactions as (
 ),
 
 no_interactions as (
-    select evt_tx_hash
-    from gnosis_protocol_v2_ethereum.GPv2Settlement_evt_Settlement
-    where evt_block_time between '{{StartTime}}' and '{{EndTime}}'
-    and evt_tx_hash not in (
+    select tx_hash
+    from cow_protocol_ethereum.batches
+    where block_time between '{{StartTime}}' and '{{EndTime}}'
+    and tx_hash not in (
         select evt_tx_hash
         from gnosis_protocol_v2_ethereum.GPv2Settlement_evt_Interaction
         where evt_block_time between '{{StartTime}}' and '{{EndTime}}'
@@ -30,17 +29,17 @@ no_interactions as (
 
 batch_interaction_counts as (
     select
-        s.evt_tx_hash,
+        tx_hash,
         count(*) as num_interactions,
         sum(case when risk_free = true then 1 else 0 end) as risk_free
-    from gnosis_protocol_v2_ethereum.GPv2Settlement_evt_Settlement s
+    from cow_protocol_ethereum.batches b
     inner join gnosis_protocol_v2_ethereum.GPv2Settlement_evt_Interaction i
-        on s.evt_tx_hash = i.evt_tx_hash
+        on tx_hash = i.evt_tx_hash
     inner join interactions i2
         on i.selector = i2.selector
         and i.target = i2.target
-    where s.evt_block_time between '{{StartTime}}' and '{{EndTime}}'
-    group by s.evt_tx_hash
+    where b.block_time between '{{StartTime}}' and '{{EndTime}}'
+    group by tx_hash
 ),
 
 combined_results as (
@@ -49,4 +48,4 @@ combined_results as (
     select *, 0 as num_interactions, 0 as risk_free from no_interactions
 )
 
-select evt_tx_hash as tx_hash from combined_results
+select tx_hash from combined_results
