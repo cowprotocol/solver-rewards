2d7760822f9700d52b308114e9f34850c92551fe
Ben Smith <bh2smith@gmail.com>
2023-01-05 13:07:53 +0100

    testing full processing method on two solvers - with both slippage signatures

--- tests/unit/test_split_transfer.py
+++ tests/unit/test_split_transfer.py
@@ -4,7 +4,7 @@ from dune_client.types import Address
 
 from src.constants import COW_TOKEN_ADDRESS
 from src.models.accounting_period import AccountingPeriod
-from src.models.slippage import SolverSlippage
+from src.models.slippage import SolverSlippage, SplitSlippages
 from src.models.split_transfers import SplitTransfers
 from src.models.token import Token
 from src.models.transfer import Transfer
@@ -86,6 +86,128 @@ class TestSplitTransfers(unittest.TestCase):
                     receiver=redirect_map[self.solver].reward_target,
                     amount_wei=cow_reward,
                 )
+            ],
+        )
+
+    def test_full_process(self):
+        """
+        This scenario involves two solvers, one with positive and one with negative slippage.
+        The negative slippage does not involve any overdraft!
+        All amounts (execution costs, cow rewards and slippage) are declared at the top of the test.
+        The expected outcome is
+        - 3 ETH transfers:
+            - 2 for execution costs with one having negative slippage deducted.
+            - 1 for positive slippage (redirected to the reward target)
+        - 2 COW transfers (one for each solver).
+        """
+        solvers = [
+            Address.from_int(1),
+            Address.from_int(2),
+        ]
+        eth_amounts = [
+            2 * ONE_ETH,
+            3 * ONE_ETH,
+        ]
+        cow_rewards = [
+            600 * ONE_ETH,
+            100 * ONE_ETH,
+        ]
+        slippage_amounts = [
+            1 * ONE_ETH,
+            -1 * ONE_ETH,
+        ]
+
+        accounting = SplitTransfers(
+            self.period,
+            mixed_transfers=[
+                Transfer(
+                    token=None,
+                    receiver=solvers[0],
+                    amount_wei=eth_amounts[0],
+                ),
+                Transfer(
+                    token=None,
+                    receiver=solvers[1],
+                    amount_wei=eth_amounts[1],
+                ),
+                Transfer(
+                    token=self.cow_token, receiver=solvers[0], amount_wei=cow_rewards[0]
+                ),
+                Transfer(
+                    token=self.cow_token, receiver=solvers[1], amount_wei=cow_rewards[1]
+                ),
+            ],
+            log_saver=PrintStore(),
+        )
+        reward_targets = [
+            Address.from_int(3),
+            Address.from_int(4),
+        ]
+        redirect_map = {
+            solvers[0]: Vouch(
+                solver=solvers[0],
+                reward_target=reward_targets[0],
+                bonding_pool=Address.zero(),
+            ),
+            solvers[1]: Vouch(
+                solver=solvers[1],
+                reward_target=reward_targets[1],
+                bonding_pool=Address.zero(),
+            ),
+        }
+        accounting.process(
+            slippage=SplitSlippages.from_data_set(
+                [
+                    {
+                        "eth_slippage_wei": slippage_amounts[0],
+                        "solver_address": solvers[0].address,
+                        "solver_name": "irrelevant1",
+                    },
+                    {
+                        "eth_slippage_wei": slippage_amounts[1],
+                        "solver_address": solvers[1].address,
+                        "solver_name": "irrelevant2",
+                    },
+                ]
+            ),
+            cow_redirects=redirect_map,
+        )
+        # Although we haven't called process_native_transfers, we are appending positive slippage inside
+        self.assertEqual(
+            accounting.eth_transfers,
+            [
+                Transfer(
+                    token=None,
+                    receiver=solvers[0],
+                    amount_wei=eth_amounts[0],
+                ),
+                Transfer(
+                    token=None,
+                    receiver=solvers[1],
+                    # Slippage is negative (so it is added here)
+                    amount_wei=eth_amounts[1] + slippage_amounts[1],
+                ),
+                Transfer(
+                    token=None,
+                    # The solver with positive slippage!
+                    receiver=redirect_map[solvers[0]].reward_target,
+                    amount_wei=slippage_amounts[0],
+                ),
+            ],
+        )
+        self.assertEqual(
+            accounting.cow_transfers,
+            [
+                Transfer(
+                    token=self.cow_token,
+                    receiver=redirect_map[solvers[0]].reward_target,
+                    amount_wei=cow_rewards[0],
+                ),
+                Transfer(
+                    token=self.cow_token,
+                    receiver=redirect_map[solvers[1]].reward_target,
+                    amount_wei=cow_rewards[1],
+                ),
             ],
         )
 
