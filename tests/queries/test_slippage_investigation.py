537cd53c6fa40d4251153e77faa2fb706ec3e925
Ben Smith <bh2smith@gmail.com>
2023-01-05 15:14:33 +0100

    add positive slippage query test

--- tests/queries/test_slippage_investigation.py
+++ tests/queries/test_slippage_investigation.py
@@ -66,6 +66,35 @@ class TestDuneAnalytics(unittest.TestCase):
         for obj in top_five_negative + top_five_positive:
             assert abs(int(obj["eth_slippage_wei"])) < 1 * 10**18
 
+    def test_positive_slippage_evaluation(self):
+        """
+        Transaction 0x5c4e410ce5d741f60e06a8621c6f12839ad39273f5abf78d4bbc1cd3b31de46c
+        Alerted on January 1, 2023.
+        https://dune.com/queries/1688044?MinAbsoluteSlippageTolerance=100&TxHash=0x&RelativeSlippageTolerance=1.0&SignificantSlippageValue=2000&StartTime=2023-01-01+00%3A00%3A00&EndTime=2023-01-02+00%3A00%3A00&EndTime_d83555=2023-01-02+00%3A00%3A00&MinAbsoluteSlippageTolerance_n26d66=100&RelativeSlippageTolerance_n26d66=1.0&SignificantSlippageValue_n26d66=2000&StartTime_d83555=2023-01-01+00%3A00%3A00&TxHash_t6c1ea=0x
+        """
+        period = AccountingPeriod("2023-01-01", 1)
+        query = QUERIES["PERIOD_SLIPPAGE"].with_params(
+            period.as_query_params()
+            + [
+                # Default values (on the query definition) do not need to be provided!
+                QueryParameter.text_type("TxHash", "0x5c4e410ce5d741f60e06a8621c6f12839ad39273f5abf78d4bbc1cd3b31de46c"),
+                # QueryParameter.text_type("SolverAddress", "0x97ec0a17432d71a3234ef7173c6b48a2c0940896"),
+                # QueryParameter.text_type("TokenList", ",".join(get_trusted_tokens())),
+                QueryParameter.text_type("CTE_NAME", "results_per_tx")
+            ],
+            dune_version=DuneVersion.V2,
+        )
+        results = exec_or_get(self.dune, query, result_id="01GP11D7FH4WAEFW1Z46Q79VBC")
+        tx_slippage = results.get_rows()[0]
+        self.assertEqual(tx_slippage["eth_slippage_wei"], 148427839329771300)
+        self.assertAlmostEqual(tx_slippage["usd_value"], 177.37732880251593, delta=0.000000001)
+        # When looking at the pure batch token imbalance:
+        # https://dune.com/queries/1380984?TxHash=0x5c4e410ce5d741f60e06a8621c6f12839ad39273f5abf78d4bbc1cd3b31de46c
+        # One sees 5 tokens listed. However, this calculation merges ETH/WETH together
+        # as a single row one of those imbalances was excluded as an "internal trade"
+        # https://dune.com/queries/1836718?CTE_NAME_e15077=final_token_balance_sheet&EndTime_d83555=2023-01-02+00%3A00%3A00&StartTime_d83555=2023-01-01+00%3A00%3A00&TxHash_t6c1ea=0x5c4e410ce5d741f60e06a8621c6f12839ad39273f5abf78d4bbc1cd3b31de46c
+        self.assertEqual(tx_slippage["num_entries"], 4)
+
 
 if __name__ == "__main__":
     unittest.main()
