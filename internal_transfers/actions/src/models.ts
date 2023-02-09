128b0ad2b3088a1ff08a2d7348f070e7c3849813
Ben Smith <bh2smith@gmail.com>
2023-02-09 10:31:04 +0100

    move comment up since true for all

--- internal_transfers/actions/src/models.ts
+++ internal_transfers/actions/src/models.ts
@@ -1,5 +1,8 @@
+// Event data declared here consists of only the relevant fields
+// for our purposes at this time.
+// For example, this program only requires the owner of the trade so
+// that transfers can be classified AMM_{IN/OUT} or USER_{IN/OUT}.
 export interface TradeEvent {
-  // The only relevant field for our purposes at this time.
   owner: string;
 }
 export interface TransferEvent {
