c8d7eaf81f54f5ad63aea6b16aa257ea85f2bf73
Ben Smith <bh2smith@gmail.com>
2023-03-28 10:09:46 +0200

    clean up and query both api endpoints

--- internal_transfers/actions/src/orderbook.ts
+++ internal_transfers/actions/src/orderbook.ts
@@ -2,48 +2,23 @@ import { WinningSettlementData } from "./models";
 import axios from "axios";
 
 // https://api.cow.fi/docs/#/
-const ORDERBOOK_API_URL =
+const PROD_API_URL =
   "https://api.cow.fi/mainnet/api/v1/solver_competition/by_tx_hash";
-// I think we should exclude Barn (or have a seprate DB for this)
-// const BARN_ORDERBOOK_API_URL =
-//   "https://barn.api.cow.fi/mainnet/api/v1/solver_competition/by_tx_hash";
+const BARN_API_URL =
+  "https://barn.api.cow.fi/mainnet/api/v1/solver_competition/by_tx_hash";
 
-// Fetches solver_competition for `tx_hash` from orderbook API via:
-// api.cow.fi/mainnet/api/v1/solver_competition/by_tx_hash/{tx_hash}
-// Parses only the relevant fields from the results and returns this info.
-/**
- * @param {string}  txHash - transaciton hash of a settlement event.
- * @returns {WinningSettlementData}
- */
-export async function getSettlementCompeitionData(
-  txHash: string
+async function getFromOrderbookAPI(
+  apiUrl: string
 ): Promise<WinningSettlementData | undefined> {
-  // TODO (the following are nice to have, but would affect performance of workflow)
-  // 1. assert isValidTxHash (regex:  /^0x([A-Fa-f0-9]{64})$/ )
-  // 2. query orderbook API
-  // 3. assert isActualTxHash: web3.eth.getTransactionReceipt(txHash) -- Requires EVM Node.
-  // 4. assert isSettlementTransaction(txReceipt)
-  // Note that, its cheaper and faster to query orderbook API for the data we want.
-  // If its not there, Prod then Barn, we could attempt to validate that the txHash was legit.
-
-  // Check Prod First
-  const url = `${ORDERBOOK_API_URL}/${txHash}`;
   try {
-    const response = await axios.get(url);
-    if (response.status === 404) {
-      // TODO steps 3 and 4 above (if we want to)
-      throw `Invalid settlement transaction ${txHash}`;
-    }
-    // console.log(`received response ${JSON.stringify(response.data, null, 2)}`);
+    const response = await axios.get(apiUrl);
 
     // The Orderbook, stores all solution submissions sorted by the objective criteria.
     // The winning solution is the last entry of the `solutions` array.
     const solutionArray = response.data.solutions;
-
     const winningSolution = solutionArray[solutionArray.length - 1];
-    // console.log(JSON.stringify(winningSolution, null, 2))
-
-    // Note that old records have solver = ZeroAddress! We need to rectify this (use txReceipt!)
+    // Note that old records have solver = ZeroAddress!
+    // We need to rectify this (use txReceipt!)
     const solver = winningSolution.solverAddress;
 
     return {
@@ -54,14 +29,39 @@ export async function getSettlementCompeitionData(
     };
   } catch (exception) {
     if (axios.isAxiosError(exception)) {
-      // Not Found.
-      // WTF is wrong with this? status is always 404 but I can't get it.
-      // console.log(JSON.stringify(exception, null, 2))
-      // console.log(exception.status)
-      // console.log(`Not Found ${JSON.stringify(exception)}`);
+      if (exception.response?.status != 404) {
+        throw `ERROR response status from ${apiUrl}: ${exception.response}`;
+      }
+      // Not Found (404) is an expected response from the API.
+      // Could be for any of the following reasons:
+      // 1. !isValidTxHash (regex:  /^0x([A-Fa-f0-9]{64})$/ )
+      // 2. !isActualTxHash: web3.eth.getTransactionReceipt(txHash) -- Requires EVM Node.
+      // 3. !isSettlementTransaction(txReceipt)
+      // 4. !correctDB
+      // TODO - the above checks would be nice to have, but would affect performance of workflow
+      // its cheaper and faster to query orderbook API for the data we want.
       return undefined;
     } else {
-      throw `ERROR received from ${url}: ${exception}`;
+      throw `ERROR unexpected response received from ${apiUrl}: ${exception}`;
     }
   }
+}
+
+/** Fetches solver_competition for `tx_hash` from orderbook API via:
+ * api.cow.fi/mainnet/api/v1/solver_competition/by_tx_hash/{tx_hash}
+ * Parses only the relevant fields from the results and returns this info.
+ *
+ * @param {string}  txHash - transaction hash of a settlement event.
+ * @returns {WinningSettlementData}
+ */
+export async function getSettlementCompetitionData(
+  txHash: string
+): Promise<WinningSettlementData | undefined> {
+  let result = await getFromOrderbookAPI(`${PROD_API_URL}/${txHash}`);
+  console.log("HELLO", result, result == undefined);
+
+  if (result == undefined) {
+    result = await getFromOrderbookAPI(`${BARN_API_URL}/${txHash}`);
+  }
+  return result;
 }
