ef2ea8aa134a113987be3c26b2ba3054d4550bce
Ben Smith <bh2smith@gmail.com>
2023-04-11 18:19:30 +0200

    partial implementation of getInternalImbalance

---
+++ internal_transfers/actions/src/accounting.ts
@@ -0,0 +1,68 @@
+import { address as SETTLEMENT_CONTRACT_ADDRESS } from "@cowprotocol/contracts/deployments/mainnet/GPv2Settlement.json";
+import { getSettlementCompetitionData } from "./orderbook";
+import { TransactionSimulator } from "./simulate/interface";
+import assert from "assert";
+import { partitionEventLogs } from "./parse";
+import { Log } from "@tenderly/actions";
+import { TokenImbalance } from "./models";
+import { aggregateTransfers } from "./utils";
+
+// This represents the only required fields for InternalImbalance accounting
+// An object of this shape can be easily be constructed from any of
+// - ethers - `TransactionReceipt`,
+// - tenderly - `TransactionEvent`
+export interface MinimalTxData {
+  from: string;
+  hash: string;
+  logs: Log[];
+}
+export async function getInternalImbalance(
+  transaction: MinimalTxData,
+  simulator: TransactionSimulator
+): Promise<TokenImbalance[]> {
+  const solverAddress = transaction.from;
+  const competition = await getSettlementCompetitionData(transaction.hash);
+  if (competition === undefined) {
+    throw Error(`No competition found for ${transaction.hash}`);
+  }
+
+  // This is more of a monitoring system task!
+  assert(
+    solverAddress === competition.solver,
+    `Winning solver ${competition.solver} doesn't match settlement solver ${solverAddress}`
+  );
+  if (competition.fullCallData === undefined) {
+    // Settlement was not even partially internalized. Return before simulating!
+    return [];
+  }
+  const simulation = await simulator.simulate({
+    callData: competition.fullCallData,
+    contractAddress: SETTLEMENT_CONTRACT_ADDRESS,
+    sender: solverAddress,
+    value: "0",
+    blockNumber: competition.simulationBlock + 1,
+  });
+  // TODO - figure out what happens when simulation fails and handle it!
+
+  const { transfers: simTransfers } = partitionEventLogs(simulation.logs);
+  const { transfers: actualTransfers } = partitionEventLogs(transaction.logs);
+
+  console.log(
+    `Found ${actualTransfers.length} relevant Actual Transfers and ${simTransfers.length} relevant Simulated Transfers`
+  );
+
+  // TODO - take the aggregated difference of (Simulated - Actual) Transfers!
+  const simulationImbalance = aggregateTransfers(
+    simTransfers,
+    SETTLEMENT_CONTRACT_ADDRESS
+  );
+  const actualImbalance = aggregateTransfers(
+    actualTransfers,
+    SETTLEMENT_CONTRACT_ADDRESS
+  );
+
+  console.log(simulationImbalance);
+  console.log(actualImbalance);
+
+  return [];
+}
