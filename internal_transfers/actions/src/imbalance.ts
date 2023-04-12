bc76521b62e8d8b8365a31de54bb36ea51742a10
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-04-12 13:22:17 +0200

    [Easy] Token Imbalance Arithmetic (#234)
    * introducing imbalanceMapDiff with tests
    * move aggregateTransfers method to imbalance.ts
    * test demonstrating exclusion of zeros

---
+++ internal_transfers/actions/src/imbalance.ts
@@ -0,0 +1,59 @@
+import { TokenImbalance, TransferEvent } from "./models";
+
+export type ImbalanceMap = Map<string, bigint>;
+
+/**
+ * Aggregation operator transforming a list of `TransferEvent` into an
+ * aggregation of token imbalances as an `ImbalanceMap`.
+ * Imbalances are defined in reference to `focalAccount`
+ * @param transfers - a list of token transfers
+ * @param focalAccount - account for which the "imbalance" is referring to.
+ */
+export function aggregateTransfers(
+  transfers: TransferEvent[],
+  focalAccount: string
+): ImbalanceMap {
+  let accumulator: ImbalanceMap = new Map<string, bigint>();
+  transfers.map((transfer) => {
+    const { to, from, amount, token } = transfer;
+
+    const currVal = accumulator.get(token) ?? 0n;
+    if (to.toLowerCase() == focalAccount.toLowerCase()) {
+      // Incoming Transfer
+      accumulator.set(token, currVal + amount);
+    } else if (from.toLowerCase() == focalAccount.toLowerCase()) {
+      // Outgoing Transfer
+      accumulator.set(token, currVal - amount);
+    } else {
+      // Irrelevant transfer.
+    }
+  });
+  return accumulator;
+}
+
+/**
+ * Difference operator on two token imbalance mappings.
+ * More generally, this computes the difference of two mappings of type { string => bigint }.
+ * Example: ({ "a": 1, "b": 2 }, { "b": 3, "c": 4 }) --> { "a": 1, "b": -1, "c": -4 }
+ * @param mapA positive term - being subtracted from
+ * @param mapB negative term - being subtracted
+ * @return mapA - mapB as a list of TokenImbalance
+ */
+export function imbalanceMapDiff(
+  mapA: ImbalanceMap,
+  mapB: ImbalanceMap
+): TokenImbalance[] {
+  const keySet = new Set([...mapA.keys(), ...mapB.keys()]);
+  let diffMap: ImbalanceMap = new Map<string, bigint>();
+  for (const token of keySet) {
+    const difference = (mapA.get(token) ?? 0n) - (mapB.get(token) ?? 0n);
+    if (difference !== 0n) {
+      // No point in recording zeros!
+      diffMap.set(token, difference);
+    }
+  }
+  return Array.from(diffMap).map(([token, amount]) => ({
+    token,
+    amount,
+  }));
+}
---
+++ internal_transfers/actions/tests/imbalance.spec.ts
@@ -0,0 +1,88 @@
+import { aggregateTransfers, imbalanceMapDiff } from "../src/imbalance";
+import { TransferEvent } from "../src/models";
+import { address as SETTLEMENT_CONTRACT_ADDRESS } from "@cowprotocol/contracts/deployments/mainnet/GPv2Settlement.json";
+
+const TOKEN_ADDRESS = "SuperToken!";
+describe("aggregateTransfers(transfers, focalAccount)", () => {
+  test("works in generic setting", () => {
+    const address = "0x0000000000000000000000000000000000000001";
+    const transferEvents: TransferEvent[] = [
+      // Incoming
+      {
+        amount: BigInt("100"),
+        from: address,
+        to: SETTLEMENT_CONTRACT_ADDRESS,
+        token: TOKEN_ADDRESS,
+      },
+      // Outgoing
+      {
+        amount: BigInt("200"),
+        from: SETTLEMENT_CONTRACT_ADDRESS,
+        to: address,
+        token: TOKEN_ADDRESS,
+      },
+      // Irrelevant
+      {
+        amount: BigInt("300"),
+        from: address,
+        to: address,
+        token: TOKEN_ADDRESS,
+      },
+    ];
+
+    expect(
+      aggregateTransfers(transferEvents, SETTLEMENT_CONTRACT_ADDRESS)
+    ).toEqual(new Map([[TOKEN_ADDRESS, BigInt("-100")]]));
+  });
+});
+describe("imbalanceMapDiff(mapA, mapB)", () => {
+  test("map difference returns expected values in generic setting", () => {
+    const mapA = new Map([
+      ["a", 1n],
+      ["b", 2n],
+    ]);
+    const mapB = new Map([
+      ["b", 3n],
+      ["c", 4n],
+    ]);
+
+    expect(imbalanceMapDiff(mapA, mapB)).toEqual([
+      {
+        amount: 1n,
+        token: "a",
+      },
+      {
+        amount: -1n,
+        token: "b",
+      },
+      {
+        amount: -4n,
+        token: "c",
+      },
+    ]);
+
+    expect(imbalanceMapDiff(new Map(), new Map())).toEqual([]);
+  });
+
+  test("map difference returns expected values on empty", () => {
+    expect(imbalanceMapDiff(new Map(), new Map())).toEqual([]);
+    const testMap = new Map([["x", 1n]]);
+    expect(imbalanceMapDiff(testMap, new Map())).toEqual([
+      {
+        amount: 1n,
+        token: "x",
+      },
+    ]);
+    expect(imbalanceMapDiff(new Map(), testMap)).toEqual([
+      {
+        amount: -1n,
+        token: "x",
+      },
+    ]);
+  });
+
+  test("map difference excludes zeros", () => {
+    const testMap = new Map([["x", 1n]]);
+    expect(imbalanceMapDiff(testMap, testMap)).toEqual([]);
+  });
+});
