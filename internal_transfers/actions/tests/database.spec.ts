ea09fc3a231de56d961184ac92e7e15c78cb194c
Ben Smith <bh2smith@gmail.com>
2023-04-17 17:49:13 +0200

    add internlized_imbalances table and insertion method

--- internal_transfers/actions/tests/database.spec.ts
+++ internal_transfers/actions/tests/database.spec.ts
@@ -1,4 +1,9 @@
-import { getDB, hexToBytea, insertSettlementEvent } from "../src/database";
+import {
+  getDB,
+  hexToBytea,
+  insertSettlementEvent,
+  insertTokenImbalances,
+} from "../src/database";
 import * as process from "process";
 import { sql } from "@databases/pg";
 import ConnectionPool from "@databases/pg/lib/ConnectionPool";
@@ -10,16 +15,18 @@ const dbURL: string =
   process.env["DATABASE_URL"] ||
   "postgresql://postgres:postgres@localhost:5432/postgres";
 const db = getDB(dbURL);
-describe("insertSettlementEvent(txHash, solver)", () => {
+
+describe("test database insertion methods", () => {
   beforeEach(async () => {
     await db.query(sql`TRUNCATE TABLE settlements;`);
+    await db.query(sql`TRUNCATE TABLE internalized_imbalances;`);
   });
 
   afterAll(async () => {
     await (db as ConnectionPool).dispose();
   });
 
-  test("Inserts to DB", async () => {
+  test("insertSettlementEvent(txHash, solver)", async () => {
     const txHash =
       "0x45f52ee09622eac16d0fe27b90a76749019b599c9566f10e21e8d0955a0e428e";
     const solver = "0xc9ec550bea1c64d779124b23a26292cc223327b6";
@@ -39,5 +46,40 @@ describe("insertSettlementEvent(txHash, solver)", () => {
         tx_hash: hexToBytea(txHash),
       },
     ]);
+  });
+  test("insertImbalances(txHash, imbalances) & idempotent", async () => {
+    const txHash = "0x4321";
+    const token1 = "0x12";
+    const token2 = "0x34";
+    const imbalances = [
+      {
+        token: token1,
+        amount:
+          115792089237316195423570985008687907853269984665640564039457584007913129639935n,
+      },
+      { token: token2, amount: 5678n },
+    ];
+    await insertTokenImbalances(db, txHash, imbalances);
+
+    let results = await db.query(sql`SELECT * from internalized_imbalances;`);
+    const expectedResults = [
+      {
+        token: hexToBytea(token1),
+        amount:
+          "115792089237316195423570985008687907853269984665640564039457584007913129639935",
+        tx_hash: hexToBytea(txHash),
+      },
+      {
+        token: hexToBytea(token2),
+        amount: "5678",
+        tx_hash: hexToBytea(txHash),
+      },
+    ];
+    expect(results).toStrictEqual(expectedResults);
+
+    // Idempotency
+    await insertTokenImbalances(db, txHash, imbalances);
+    results = await db.query(sql`SELECT * from internalized_imbalances;`);
+    expect(results).toStrictEqual(expectedResults);
   });
 });
