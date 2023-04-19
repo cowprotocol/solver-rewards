596991097f0a1b34e9b71f15e0f077dc44da52e8
Ben Smith <bh2smith@gmail.com>
2023-04-19 10:01:09 +0200

    refactor event log parsing (break down into smaller components)

--- internal_transfers/actions/src/parse.ts
+++ internal_transfers/actions/src/parse.ts
@@ -1,4 +1,10 @@
-import { SettlementEvent, TradeEvent, TransferEvent } from "./models";
+import {
+  isSettlementEvent,
+  isTradeEvent,
+  SettlementEvent,
+  TradeEvent,
+  TransferEvent,
+} from "./models";
 import { Log } from "@tenderly/actions";
 import { transferInvolves } from "./utils";
 import { ethers } from "ethers";
@@ -6,6 +12,8 @@ import { abi as SETTLEMENT_ABI } from "@cowprotocol/contracts/lib/contracts/GPv2
 import { abi as IERC20_ABI } from "@cowprotocol/contracts/lib/contracts/IERC20.json";
 import { address as SETTLEMENT_CONTRACT_ADDRESS } from "@cowprotocol/contracts/deployments/mainnet/GPv2Settlement.json";
 
+const settlementInterface = new ethers.Interface(SETTLEMENT_ABI);
+const erc20Interface = new ethers.Interface(IERC20_ABI);
 export interface ClassifiedEvents {
   trades: TradeEvent[];
   transfers: TransferEvent[];
@@ -18,39 +26,26 @@ export function partitionEventLogs(logs: Log[]): ClassifiedEvents {
     trades: [],
     settlements: [],
   };
-  const settlementInterface = new ethers.Interface(SETTLEMENT_ABI);
-  const erc20Interface = new ethers.Interface(IERC20_ABI);
+
   logs.forEach((log, index) => {
     // We are only interested in Transfer Events from erc20 contracts
     // along with Settlement and Trade Events from the Settlement contract
     // All other event emissions can be ignored for our purposes.
-    const transferEventLog = erc20Interface.parseLog(log);
-    const settlementEventLog = settlementInterface.parseLog(log);
-    if (transferEventLog != null && transferEventLog.name === "Transfer") {
-      const { from, to, value } = transferEventLog.args;
-      const transfer: TransferEvent = {
-        token: log.address,
-        from,
-        to,
-        amount: BigInt(value),
-      };
-      // Is a "relevant" transfer (involving settlement contract)
+    const possibleSettlementEvent = tryParseSettlementEventLog(log, index);
+    const possibleTransfer = tryParseERC20Transfer(log);
+
+    if (possibleTransfer !== null) {
+      const transfer: TransferEvent = possibleTransfer;
       if (transferInvolves(transfer, SETTLEMENT_CONTRACT_ADDRESS)) {
         result.transfers.push(transfer);
       }
-    } else if (settlementEventLog != null) {
+    } else if (possibleSettlementEvent !== null) {
+      const settlementEventLog = possibleSettlementEvent;
       // Relevant Event Types
-      if (settlementEventLog.name == "Trade") {
-        const { owner } = settlementEventLog.args;
-        result.trades.push({
-          owner,
-        });
-      } else if (settlementEventLog.name == "Settlement") {
-        const { solver } = settlementEventLog.args;
-        result.settlements.push({
-          solver,
-          logIndex: index,
-        });
+      if (isTradeEvent(settlementEventLog)) {
+        result.trades.push(settlementEventLog);
+      } else if (isSettlementEvent(settlementEventLog)) {
+        result.settlements.push(settlementEventLog);
       } else {
         // Placeholder for other Settlement Contract Events (e.g. Interaction)
       }
@@ -60,4 +55,39 @@ export function partitionEventLogs(logs: Log[]): ClassifiedEvents {
     }
   });
   return result;
+}
+
+function tryParseSettlementEventLog(
+  log: Log,
+  index: number
+): SettlementEvent | TradeEvent | null {
+  const settlementEventLog = settlementInterface.parseLog(log);
+  if (settlementEventLog === null) return null;
+  // Relevant Event Types
+  if (settlementEventLog.name == "Trade") {
+    const { owner } = settlementEventLog.args;
+    return { owner } as TradeEvent;
+  } else if (settlementEventLog.name == "Settlement") {
+    const { solver } = settlementEventLog.args;
+    return {
+      solver,
+      logIndex: index,
+    } as SettlementEvent;
+  } else {
+    // Placeholder for other Settlement Contract Events (e.g. Interaction)
+    return null;
+  }
+}
+
+function tryParseERC20Transfer(log: Log): TransferEvent | null {
+  const transferEventLog = erc20Interface.parseLog(log);
+  if (transferEventLog === null || transferEventLog.name !== "Transfer")
+    return null;
+  const { from, to, value } = transferEventLog.args;
+  return {
+    token: log.address,
+    from,
+    to,
+    amount: BigInt(value),
+  };
 }
