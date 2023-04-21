c193959b8372bd9a444bf4888d13090ea4cd9322
Ben Smith <bh2smith@gmail.com>
2023-04-21 15:21:14 +0200

    lint sql file

--- internal_transfers/database/sql/V003__create_settlement_simulations.sql
+++ internal_transfers/database/sql/V003__create_settlement_simulations.sql
@@ -1,12 +1,12 @@
 create table settlement_simulations
 (
     -- The hash of the actual (mined) Settlement Transaction
-    tx_hash  bytea NOT NULL,
+    tx_hash            bytea NOT NULL,
     -- Relevant Settlement Data used to generate simulations
     winning_settlement jsonb NOT NULL,
     -- Structure of the SimulationData is here: internal_transfers/actions/src/simulate/interface.ts
-    complete jsonb NOT NULL,
-    reduced  jsonb NOT NULL,
+    complete           jsonb NOT NULL,
+    reduced            jsonb NOT NULL,
 
     PRIMARY KEY (tx_hash)
 );
