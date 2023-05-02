ea41d69c9e44df1e7f55795cd4e9a0697ec951bb
Ben Smith <bh2smith@gmail.com>
2023-05-02 18:04:52 -0400

    fix block range and migrate query to V3

--- queries/dune_v2/period_block_interval.sql
+++ queries/dune_v2/period_block_interval.sql
@@ -1,5 +1,6 @@
--- V2: https://dune.com/queries/1541504
-select min(number) as start_block,
-       max(number) as end_block
+-- V3: https://dune.com/queries/1541504
+select
+    min("number") as start_block,
+    max("number") as end_block
 from ethereum.blocks
-where time between '{{StartTime}}' and '{{EndTime}}'
+where date_trunc('minute', time) between cast('{{StartTime}}' as timestamp) and cast('{{EndTime}}' as timestamp)
