466340ce8417528b6854a25870f0c541bf56d93d
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-05-03 17:02:02 -0400

    Migrate Easy Dune Queries (#267)
    Just a few queries that have been converted to DuneSQL.

--- queries/dune_v2/trade_counts.sql
+++ queries/dune_v2/trade_counts.sql
@@ -1,8 +1,8 @@
--- V2 Query: https://dune.com/queries/1785586
+-- V3 Query: https://dune.com/queries/1785586
 select
     solver_address as solver,
     sum(num_trades) as num_trades
 from cow_protocol_ethereum.batches
 where block_number between {{start_block}} and {{end_block}}
-group by solver
+group by solver_address
 order by num_trades desc, solver
