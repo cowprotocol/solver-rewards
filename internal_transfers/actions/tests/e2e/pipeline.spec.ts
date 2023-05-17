1554a4350557ccc35a48127f40e34cd147cf1bbf
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-05-17 12:18:07 +0200

    [Bug] Catch Missing Competition (#281)
    Closes #280 
    
    We had a short outage due to a forked block / failed transaction (cf
    [tenderly](https://dashboard.tenderly.co/gp-v2/solver-slippage/action/4f657ddd-c054-4704-9e1e-50385ad2fc2c/execution/a14e3e0f-3e26-4290-8684-1720d74f1513)).
    
    This
    [transaction](https://etherscan.io/tx/0x1231f2c9b519adc5ae7db17c84418e140553e234b2868d6b1d7f66a692683e73)
    was previously included in a block that wound up forked (tenderly picked
    this up as successful beforehand) we stored the original successful
    transaction in our DB, but then later it turns out to be a failed
    transaction. This explains why the solver competition was not found.
    
    Instead of throwing an Error when competition is not found, we can at
    least go back to the EVM and check if the transaction details still
    match what we have. I am not exactly sure how to uniquely identify a
    "failed" transaction, but it suffices for our purposes that the
    transaction does not have any logs.
    
    
    ## Test Plan
    
    Added a test demonstrating that the pipeline does not throw when this
    scenario occurs.

--- internal_transfers/actions/tests/e2e/pipeline.spec.ts
+++ internal_transfers/actions/tests/e2e/pipeline.spec.ts
@@ -2,14 +2,17 @@ import { TenderlySimulator } from "../../src/simulate/tenderly";
 import { internalizedTokenImbalance } from "../../src/pipeline";
 import { getDB } from "../../src/database";
 import { getTxData } from "./helper";
+import { ethers } from "ethers";
 
-const { TENDERLY_USER, TENDERLY_PROJECT, TENDERLY_ACCESS_KEY } = process.env;
+const { TENDERLY_USER, TENDERLY_PROJECT, TENDERLY_ACCESS_KEY, NODE_URL } =
+  process.env;
 
 const simulator = new TenderlySimulator(
   TENDERLY_USER || "INVALID_USER",
   TENDERLY_PROJECT || "TENDERLY_PROJECT",
   TENDERLY_ACCESS_KEY || "TENDERLY_ACCESS_KEY"
 );
+const provider = ethers.getDefaultProvider(NODE_URL!);
 
 const db = getDB("postgresql://postgres:postgres@localhost:5432/postgres");
 
@@ -19,7 +22,7 @@ describe.skip("Run Full Pipeline", () => {
       "0x0f86c06d9ace6a88644db6b654a904aa62c82305023e094ce49650467c91bd6e"
     );
     await expect(
-      internalizedTokenImbalance(notInternalized, db, simulator)
+      internalizedTokenImbalance(notInternalized, db, simulator, provider)
     ).resolves.not.toThrowError();
   }, 300000);
   test("run pipeline on batch with internalized transfers", async () => {
@@ -27,16 +30,25 @@ describe.skip("Run Full Pipeline", () => {
       "0xDCD5CF12340B50ACC04DBE7E14A903BE373456C81E4DB20DD84CF0301F6AB869"
     );
     await expect(
-      internalizedTokenImbalance(internalized, db, simulator)
+      internalizedTokenImbalance(internalized, db, simulator, provider)
     ).resolves.not.toThrowError();
   }, 300000);
 
-  test("run pipeline on transaction with unavailable competition data", async () => {
+  test("throws on unavailable competition data for successful transaction", async () => {
     const txHash =
       "0xe6a0fbad3f9571e7614dbbc1d65d523cbeb6929b59bd20cde80ac791899fccfb";
     const unavailable = await getTxData(txHash);
     await expect(
-      internalizedTokenImbalance(unavailable, db, simulator)
+      internalizedTokenImbalance(unavailable, db, simulator, provider)
     ).rejects.toThrow(`No competition found for ${txHash}`);
+  }, 300000);
+
+  test("resolves on unavailable competition data for failed transaction", async () => {
+    const txHash =
+      "0x1231f2c9b519adc5ae7db17c84418e140553e234b2868d6b1d7f66a692683e73";
+    const unavailable = await getTxData(txHash);
+    await expect(
+      internalizedTokenImbalance(unavailable, db, simulator, provider)
+    ).resolves.not.toThrowError();
   }, 300000);
 });
