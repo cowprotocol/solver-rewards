bb65f3b946fdb96cf31498cc81bb14c0e5165b14
Ben Smith <bh2smith@gmail.com>
2023-06-23 11:43:47 +0200

    decode base64

--- internal_transfers/actions/lambda.ts
+++ internal_transfers/actions/lambda.ts
@@ -1,6 +1,10 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 import { Context as AwsContext } from "aws-lambda/handler";
 import { txHandler } from "./src/pipeline";
+import { Buffer } from "buffer";
+
+const decode = (str: string): string =>
+  Buffer.from(str, "base64").toString("binary");
 
 export const handler = async (
   event: APIGatewayEvent,
@@ -11,9 +15,12 @@ export const handler = async (
     dbUrl: process.env.DATABASE_URL!,
     simulatorKey: process.env.TENDERLY_ACCESS_KEY!,
   };
-  const txHash = event.body
-    ? JSON.parse(event.body).txHash
-    : (event as any).txHash;
+  let txHash: string;
+  if (event.body !== null) {
+    txHash = JSON.parse(decode(event.body)).txHash;
+  } else {
+    txHash = (event as any).txHash;
+  }
 
   await txHandler(txHash, secrets);
   return {
