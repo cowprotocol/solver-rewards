f61098624fe266fed9026b2b05406395ab4a4a7b
Felix Henneke <felix.henneke@protonmail.com>
2024-01-04 12:45:09 +0100

    remove restriction on quote rewards for limit orders
    before this change only market orders were eligible for quote rewards
    and only in that case a quote_solver was fetched.
    with this change, a quote_solver is always fetched (if it exists)

--- queries/orderbook/quote_rewards.sql
+++ queries/orderbook/quote_rewards.sql
@@ -3,8 +3,7 @@ with winning_quotes as (SELECT concat('0x', encode(oq.solver, 'hex')) as solver,
                         FROM trades t
                                INNER JOIN orders o ON order_uid = uid
                                JOIN order_quotes oq ON t.order_uid = oq.order_uid
-                        WHERE o.class = 'market'
-                          AND block_number BETWEEN {{start_block}} AND {{end_block}}
+                        WHERE block_number BETWEEN {{start_block}} AND {{end_block}}
                           AND oq.solver != '\x0000000000000000000000000000000000000000')
 
 SELECT solver, count(*) AS num_quotes
