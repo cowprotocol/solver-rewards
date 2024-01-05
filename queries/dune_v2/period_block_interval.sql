54206e1903aa832e21b81e5aa3a1cb5ee5720f81
Felix Henneke <felix.henneke@protonmail.com>
2024-01-05 12:24:37 +0100

    compute block_range consistent with half closed intervals
    With this code, one always has that end_block in week n is start_block
    in week n+1.
    If all queries follow the the format block > start_block and block <= end_block,
    each block appears in exactly one range.

--- queries/dune_v2/period_block_interval.sql
+++ queries/dune_v2/period_block_interval.sql
@@ -1,6 +1,16 @@
 -- V3: https://dune.com/queries/1541504
-select
-    min("number") as start_block,
-    max("number") as end_block
-from ethereum.blocks
-where date_trunc('minute', time) between cast('{{StartTime}}' as timestamp) and cast('{{EndTime}}' as timestamp)
+with block_range_start as (
+    select
+        max("number") as start_block
+    from ethereum.blocks
+    where time <= cast('{{StartTime}}' as timestamp)
+)
+,block_range_stop as (
+    select
+        max("number") as end_block
+    from ethereum.blocks
+    where time <= cast('{{EndTime}}' as timestamp)
+)
+select *
+from block_range_start
+cross join block_range_stop
--- queries/dune_v2/period_slippage.sql
+++ queries/dune_v2/period_slippage.sql
@@ -225,11 +225,22 @@ incoming_and_outgoing as (
     SELECT from_hex(address_str) as address
     FROM ( VALUES {{TokenList}} ) as _ (address_str)
 )
+,block_range_start as (
+    select
+        max("number") as start_block
+    from ethereum.blocks
+    where time <= cast('{{StartTime}}' as timestamp)
+)
+,block_range_stop as (
+    select
+        max("number") as end_block
+    from ethereum.blocks
+    where time <= cast('{{EndTime}}' as timestamp)
+)
 ,block_range as (
-  select min(number) as start_block,
-       max(number) as end_block
-  from ethereum.blocks
-  where time between cast('{{StartTime}}' as timestamp) and cast('{{EndTime}}' as timestamp)
+    select *
+    from block_range_start
+    cross join block_range_stop
 )
 ,internalized_imbalances as (
   select  b.block_time,
