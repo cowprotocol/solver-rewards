e3ec33a092818447a15152a46d5bad98bd0790b7
Felix Henneke <felix.henneke@protonmail.com>
2024-01-15 17:36:46 +0100

    black fix

--- tests/queries/test_quote_rewards.py
+++ tests/queries/test_quote_rewards.py
@@ -10,7 +10,9 @@ class TestQuoteRewards(unittest.TestCase):
     def setUp(self) -> None:
         db_url = "postgres:postgres@localhost:5432/postgres"
         self.fetcher = MultiInstanceDBFetcher([db_url])
-        with open("./tests/queries/quote_rewards_test_db.sql", "r", encoding="utf-8") as file:
+        with open(
+            "./tests/queries/quote_rewards_test_db.sql", "r", encoding="utf-8"
+        ) as file:
             self.fetcher.connections[0].execute(file.read())
 
     def test_get_quote_rewards(self):
