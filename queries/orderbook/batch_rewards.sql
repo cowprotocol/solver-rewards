604b4a45545d665dcf871e6ec7016e791cbc7942
Felix Henneke <felix.henneke@protonmail.com>
2024-01-19 11:40:11 +0100

    address review comments
    - add a few comments
    - reorder terms
    - fix typos

--- queries/orderbook/batch_rewards.sql
+++ queries/orderbook/batch_rewards.sql
@@ -35,17 +35,17 @@ order_surplus AS (
         at.auction_id,
         s.tx_hash,
         t.order_uid,
-        o.sell_token, -- the total amount the user sends
+        o.sell_token,
         o.buy_token,
-        t.sell_amount,
-        t.buy_amount,
-        oe.surplus_fee as observed_fee,
+        t.sell_amount, -- the total amount the user sends
+        t.buy_amount, -- the total amount the user receives
+        oe.surplus_fee as observed_fee, -- the total discrepancy between what the user sends and what they would have send if they traded at clearing price
         o.kind,
         CASE
             WHEN o.kind = 'sell'
-                THEN t.buy_amount - o.buy_amount / (o.sell_amount + o.fee_amount) * t.sell_amount
+                THEN t.buy_amount - t.sell_amount * o.buy_amount / (o.sell_amount + o.fee_amount)
             WHEN o.kind = 'buy'
-                THEN (o.sell_amount + o.fee_amount) / o.buy_amount * t.buy_amount - t.sell_amount
+                THEN t.buy_amount * (o.sell_amount + o.fee_amount) / o.buy_amount - t.sell_amount
         END AS surplus,
         CASE
             WHEN o.kind = 'sell'
@@ -72,7 +72,7 @@ order_surplus AS (
         os.auction_id,
         os.solver,
         os.tx_hash,
-        os.sell_amount, -- the total amount the user sends
+        os.sell_amount,
         os.buy_amount,
         os.sell_token,
         os.observed_fee,
@@ -85,7 +85,11 @@ order_surplus AS (
                         WHEN os.kind = 'sell'
                             THEN
                                 CASE
-                                    WHEN fp.max_volume_factor = 1 -- this is done to avoid a division by zero bug
+                                    WHEN fp.max_volume_factor = 1 -- this is done to avoid a division by zero errors
+                                    -- We assume that the case surplus_factor != 1 always. In
+                                    -- that case reconstructing the protocol fee would be
+                                    -- impossible anyways. This query will return a division by
+                                    -- zero error in that case.
                                         THEN fp.surplus_factor / (1 - fp.surplus_factor) * surplus
                                     ELSE
                                         LEAST(
@@ -194,8 +198,8 @@ batch_protocol_fees AS (
                                    solver,
                                    execution_cost,
                                    surplus,
-                                   protocol_fee,
-                                   fee + network_fee_correction,
+                                   fee - coalesce(network_fee_correction, 0) as fee, -- an estimated network fee
+                                   coalesce(protocol_fee, 0) as protocol_fee, -- the protocol fee
                                    surplus + coalesce(protocol_fee - network_fee_correction, 0) + fee - reference_score as uncapped_reward_eth,
                                    -- Uncapped Reward = CLAMP_[-E, E + exec_cost](uncapped_reward_eth)
                                    LEAST(GREATEST(-{{EPSILON}}, surplus + coalesce(protocol_fee - network_fee_correction, 0) + fee - reference_score),
