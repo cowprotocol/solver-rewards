60a358e2f2941fc26505b46145bbebcabd75b27a
Felix Henneke <felix.henneke@protonmail.com>
2024-05-24 12:34:15 +0200

    black fix

--- tests/unit/test_slippage.py
+++ tests/unit/test_slippage.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-    # TODO: Implement these tests, see issue #361
+# TODO: Implement these tests, see issue #361
 class TestSlippage(unittest.TestCase):
     def test_compute_slippage(self):
         pass
