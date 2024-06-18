cbef996c022a6fe3ac01a49176c29d38cff3e706
harisang <harisangelidakis@gmail.com>
2024-06-18 13:43:06 +0300

    add back symbol in query

--- queries/dune_v2/period_slippage.sql
+++ queries/dune_v2/period_slippage.sql
@@ -320,6 +320,7 @@ block_range as (
         block_time,
         tx_hash,
         solver_address,
+        symbol,
         CASE
             WHEN token = 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee then 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
             ELSE token
