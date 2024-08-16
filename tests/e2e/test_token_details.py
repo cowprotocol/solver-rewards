cbda30b50895541352f33e9b38b72cb1ab6ea93c
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-08-16 13:31:34 +0200

    fix token tests

--- tests/e2e/test_token_details.py
+++ tests/e2e/test_token_details.py
@@ -5,18 +5,17 @@ from web3 import Web3
 
 from src.utils.token_details import get_token_decimals
 
-W3 = Web3(Web3.HTTPProvider("https://rpc.ankr.com/eth_goerli"))
+W3 = Web3(Web3.HTTPProvider("https://rpc.ankr.com/eth_sepolia"))
 
 
 class TestTokenDecimals(unittest.TestCase):
     def test_token_decimals(self):
-        # Goerli doesn't seem to have tokens with anything other than 18 decimals.
-        cow = "0x3430d04E42a722c5Ae52C5Bffbf1F230C2677600"
-        self.assertEqual(get_token_decimals(W3, cow), 18)
-        self.assertEqual(get_token_decimals(W3, Address(cow)), 18)
+        usdc = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
+        self.assertEqual(get_token_decimals(W3, usdc), 6)
+        self.assertEqual(get_token_decimals(W3, Address(usdc)), 6)
 
     def test_token_decimals_cache(self):
-        new_token = "0x40c92339fd9c0f59d976af127e82de61914efd0f"
+        new_token = "0x0625afb445c3b6b7b929342a04a22599fd5dbb59"
         with self.assertLogs("src.utils.token_details", level="INFO"):
             get_token_decimals(W3, new_token)
 
