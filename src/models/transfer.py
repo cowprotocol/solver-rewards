70c3d455e6b7d72dbfda0f9edf5e98bcbb40d4a2
harisang <harisangelidakis@gmail.com>
2024-10-07 15:12:59 +0300

    restore transfer assertion

--- src/models/transfer.py
+++ src/models/transfer.py
@@ -52,6 +52,8 @@ class Transfer:
     amount_wei: int
 
     def __init__(self, token: Optional[Token], recipient: Address, amount_wei: int):
+        assert amount_wei > 0, f"Can't construct non-positive transfer of {amount_wei}"
+
         self.token = token
         self._recipient = recipient
         self.amount_wei = amount_wei
