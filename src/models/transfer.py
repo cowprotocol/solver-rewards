07390301c75d74fcca464a0ecd2b9cb21caec987
Felix Henneke <felix.henneke@protonmail.com>
2024-11-05 14:23:56 +0100

    remove merge and consolidate function
    those functions are note used after removing the command line option

--- src/models/transfer.py
+++ src/models/transfer.py
@@ -99,26 +99,6 @@ class Transfer:
         """Read access to the recipient of a transfer"""
         return self._recipient
 
-    @staticmethod
-    def consolidate(transfer_list: list[Transfer]) -> list[Transfer]:
-        """
-        Removes redundancy of a transfer list by consolidating _duplicate transfers_.
-        Duplicates defined as transferring the same token to one recipient multiple times.
-        This optimizes gas cost of multiple transfers.
-        """
-
-        transfer_dict: dict[tuple, Transfer] = {}
-        for transfer in transfer_list:
-            key = (transfer.recipient, transfer.token)
-            if key in transfer_dict:
-                transfer_dict[key] = transfer_dict[key].merge(transfer)
-            else:
-                transfer_dict[key] = transfer
-        return sorted(
-            transfer_dict.values(),
-            key=lambda t: (-t.amount, t.recipient, t.token),
-        )
-
     @property
     def token_type(self) -> TokenType:
         """Returns the type of transfer (Native or ERC20)"""
@@ -134,29 +114,6 @@ class Transfer:
         # This case was handled above.
         assert self.token is not None
         return self.amount_wei / int(10**self.token.decimals)
-
-    def merge(self, other: Transfer) -> Transfer:
-        """
-        Merge two transfers (acts like addition) if all fields except amount are equal,
-        returns a transfer who amount is the sum.
-        Merges incur information loss (particularly in the category of redirects).
-        Note that two transfers of the same token with different receivers,
-        but redirected to the same address, will get merged and original receivers will be forgotten
-        """
-        merge_requirements = [
-            self.recipient == other.recipient,
-            self.token == other.token,
-        ]
-        if all(merge_requirements):
-            return Transfer(
-                token=self.token,
-                recipient=self.recipient,
-                amount_wei=self.amount_wei + other.amount_wei,
-            )
-        raise ValueError(
-            f"Can't merge transfers {self}, {other}. "
-            f"Requirements met {merge_requirements}"
-        )
 
     def as_multisend_tx(self) -> MultiSendTx:
         """Converts Transfer into encoded MultiSendTx bytes"""
