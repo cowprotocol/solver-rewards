1c62c500b63b779773319eb6e26cf8d2ce1e612f
= <bram@cow.fi>
2024-11-26 16:36:57 +0100

    Update reference to query

--- solver_rewards/utils/query_file.py
+++ solver_rewards/utils/query_file.py
@@ -5,6 +5,7 @@ These utilities eliminate the need to use relative paths.
 """
 
 import os
+import importlib
 
 from solver_rewards.config import IOConfig
 
@@ -13,7 +14,7 @@ io_config = IOConfig.from_env()
 
 def open_query(filename: str) -> str:
     """Opens `filename` and returns as string"""
-    with open(query_file(filename), "r", encoding="utf-8") as file:
+    with importlib.resources.open_text('solver_rewards.sql.orderbook', filename) as file:
         return file.read()
 
 
@@ -21,11 +22,6 @@ def open_dashboard_query(filename: str) -> str:
     """Opens `filename` from DASHBOARD_PATH and returns as string"""
     with open(dashboard_file(filename), "r", encoding="utf-8") as file:
         return file.read()
-
-
-def query_file(filename: str) -> str:
-    """Returns proper path for filename in QUERY_PATH"""
-    return os.path.join(io_config.query_dir, filename)
 
 
 def dashboard_file(filename: str) -> str:
