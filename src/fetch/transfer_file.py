cc463405fd361028c12511de51cad1c69bce7e18
harisang <harisangelidakis@gmail.com>
2024-12-24 16:35:15 +0200

    add default min transfer amounts per network

--- src/fetch/transfer_file.py
+++ src/fetch/transfer_file.py
@@ -113,9 +113,9 @@ def auto_propose(
 def main() -> None:
     """Generate transfers for an accounting period"""
 
-    args = generic_script_init(description="Fetch Complete Reimbursement")
-
     config = AccountingConfig.from_network(Network(os.environ["NETWORK"]))
+
+    args = generic_script_init(description="Fetch Complete Reimbursement")
 
     accounting_period = AccountingPeriod(args.start)
 
