c967b6497e19f0b655a550896d6e1b71ea2dfee7
Felix Henneke <felix.henneke@protonmail.com>
2025-01-09 13:27:50 +0100

    adding more logging

--- src/fetch/orderbook.py
+++ src/fetch/orderbook.py
@@ -237,6 +237,7 @@ class OrderbookFetcher:
         Data is upserted: it is inserted if possible (i.e. it does not yet exist) and updated
         otherwise."""
         if recreate_table:
+            log.info(f"(Re)creating table {table_name}.")
             data = new_data
         else:
             # set index for upserting data depending on type of data
@@ -258,9 +259,11 @@ class OrderbookFetcher:
                     index_col=index_cols,
                 ).drop("index", axis=1)
                 # upsert data (insert if possible, otherwise update)
+                log.info(f"Upserting into table {table_name}.")
                 data = pd.concat([data[~data.index.isin(new_data.index)], new_data])
             except ValueError:  # this catches the case of a missing table
                 data = new_data
+                log.info(f"Creating new table {table_name}.")
         # reset index for compatibility with the current integer index
         # this can be removed to not have the unnecessary index column
         data = data.reset_index()
