137d49da89dc4f130b47f346b7aa71a4dfb7496b
harisang <harisangelidakis@gmail.com>
2025-01-10 01:29:53 +0200

    bug fixes

--- src/fetch/payouts.py
+++ src/fetch/payouts.py
@@ -600,8 +600,9 @@ def construct_payouts(
         f"Network Fees: {total_network_fees / 10**18:.4f}\n"
         f"Slippage: {total_slippage / 10**18:.4f}\n\n"
         f"Exchange rate native token to COW: {exchange_rate_native_to_cow:.6g} COW/native token\n"
-        f"Exchange rate native token to ETH: {exchange_rate_native_to_eth:.6g} ETH/native token\n",
-        f"Minimum native token transfer: {config.payment_config.min_native_token_transfer / 10**18} units\n",
+        f"Exchange rate native token to ETH: {exchange_rate_native_to_eth:.6g} ETH/native token\n"
+        f"""Minimum native token transfer: 
+        {config.payment_config.min_native_token_transfer / 10**18} units\n"""
         f"Minimum COW transfer: {config.payment_config.min_cow_transfer / 10**18} units\n",
         category=Category.TOTALS,
     )
--- src/fetch/transfer_file.py
+++ src/fetch/transfer_file.py
@@ -102,9 +102,10 @@ def auto_propose(
             slack_client,
             channel=slack_channel,
             message=(
-                f"Solver Rewards transaction for network {config.dune_config.dune_blockchain} with nonce {nonce} pending signatures.\n"
-                f"To sign and execute, visit:\n{config.payment_config.safe_queue_url}\n"
-                f"More details in thread"
+                f"""Solver Rewards transaction for network {config.dune_config.dune_blockchain} 
+                with nonce {nonce} pending signatures.\n"
+                To sign and execute, visit:\n{config.payment_config.safe_queue_url}\n"
+                More details in thread"""
             ),
             sub_messages=log_saver_obj.get_values(),
         )
