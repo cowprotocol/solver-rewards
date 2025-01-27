58fdf14625cf2d5687000a366cf44539517941b7
Felix Henneke <felix.henneke@protonmail.com>
2025-01-27 10:34:40 +0100

    Restructure payouts file (#435)
    This PR is an attempt at implementing #427. The PR does not change the
    values of final results (up to rounding of floats). It changes the
    structure of the code and intermediate representations.
    
    As a first step, it separates the computation of different parts of the
    accounting into different functions.
    
    ```python
        # compute individual components of payments
        solver_info = compute_solver_info(
            reward_target_df,
            service_fee_df,
            config,
        )
        rewards = compute_rewards(
            batch_data,
            quote_rewards_df,
            exchange_rate_native_to_cow,
            config.reward_config,
        )
        protocol_fees = compute_protocol_fees(batch_data)
        partner_fees = compute_partner_fees(batch_data, config.protocol_fee_config)
        buffer_accounting = compute_buffer_accounting(batch_data, slippage_df)
    ```
    Those functions are implemented in separate files 
    
    The results of these steps are converted into data frames for solver
    payments and for protocol and partner fee payments.
    
    ```python
        # combine into solver payouts and partner payouts
        solver_payouts = compute_solver_payouts(
            solver_info, rewards, protocol_fees, buffer_accounting
        )
        partner_payouts = (
            partner_fees  # no additional computation required here at the moment
        )
    ```
    
    Payout data on transfers and overdrafts is then computed from solver and
    parner payouts data.
    
    ```python
        payouts = prepare_payouts(solver_payouts, partner_payouts, dune.period, config)
    ```
    
    The code can be tested to produce the same transfer files as the old
    code.
    Tests have been adapted and cover essentially what was tested before.
    There is a bit more strictness in the testing of the separate
    computations of rewards, protocol fees, partner fees, etc.
    There is no end-to-end test for payments yet. This should be added at
    some point.
    
    Future changes could remove data frames from intermediate results. This
    would make it easier to have correct types and detect and handle missing
    data. Data for the different parts of the accounting can be changed to
    use intermediate tables generated by `src/data_sync/sync_data.py`.
    
    ---------
    
    Co-authored-by: Haris Angelidakis <64154020+harisang@users.noreply.github.com>

---
+++ src/fetch/partner_fees.py
@@ -0,0 +1,136 @@
+"""Functionality for partner fees."""
+
+from collections import defaultdict
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+
+from src.config import ProtocolFeeConfig
+
+BATCH_DATA_COLUMNS = ["partner_list", "partner_fee_eth"]
+
+PARTNER_FEES_COLUMNS = ["partner", "partner_fee_eth", "partner_fee_tax"]
+
+
+def compute_partner_fees(batch_data: DataFrame, config: ProtocolFeeConfig) -> DataFrame:
+    """Compute partner fees per partner.
+
+    Parameters
+    ----------
+    batch_data : DataFrame
+        Batch rewards data.
+        The columns have to contain BATCH_DATA_COLUMNS:
+        - partner_list : list[str]
+            List of "0x"-prefixed hex representations of the partner addresses. Partner fees are
+            paid to these addresses.
+        - partner_fee_eth : list[int]
+            List of partner fees in wei a solver owes to a partner for settling batches. The list is
+            aligned with the respective partners in partner_list.
+
+    config : ProtocolFeeConfig
+        Protocol fee configuration.
+
+    Returns
+    -------
+    partner_fees : DataFrame
+        Data frame containing partner fees per partner.
+        The columns are PARTNER_FEES_COLUMNS:
+        - partner : str
+            "0x"-prefixed hex representation of the address of a partner. Partner fees are paid to
+            these addresses.
+        - partner_fee_eth : int
+            Total partner fee in wei of a partner. This amount is reduced by partner_fee_tax before
+            payment.
+        - partner_fee_tax : Fraction
+            The fraction of partner fees which need to be paid to the CoW DAO.
+
+    Raises
+    ------
+    AssertionError
+        If input dataframe does not contain required columns or if the result does not have correct
+        columns.
+    """
+
+    # validate batch data columns
+    assert set(BATCH_DATA_COLUMNS).issubset(set(batch_data.columns))
+
+    partner_fee_lists = batch_data[BATCH_DATA_COLUMNS]
+
+    partner_fees = compute_partner_fees_per_partner(partner_fee_lists, config)
+
+    assert set(partner_fees.columns) == set(PARTNER_FEES_COLUMNS)
+
+    return partner_fees
+
+
+def compute_partner_fees_per_partner(
+    partner_fee_lists: DataFrame, config: ProtocolFeeConfig
+) -> DataFrame:
+    """Compute partner fees per partner.
+
+    This is the main computation step for partner fees. It has the same input and output format as
+    `compute_partner_fees`.
+
+    Parameters
+    ----------
+    partner_fee_lists : DataFrame
+        Batch rewards data.
+        The columns are BATCH_DATA_COLUMNS:
+        - partner_list : list[str]
+            List of "0x"-prefixed hex representations of the partner addresses. Partner fees are
+            paid to these addresses.
+        - partner_fee_eth : list[int]
+            List of partner fees in wei a solver owes to a partner for settling batches. The list is
+            aligned with the respective partners in partner_list.
+
+    config : ProtocolFeeConfig
+        Protocol fee configuration.
+
+    Returns
+    -------
+    partner_fees_df : DataFrame
+        Data frame containing partner fees per partner.
+        The columns are PARTNER_FEES_COLUMNS:
+        - partner : str
+            "0x"-prefixed hex representation of the address of a partner. Partner fees are paid to
+            these addresses.
+        - partner_fee_eth : int
+            Total partner fee in wei of a partner. This amount is reduced by partner_fee_tax before
+            payment.
+        - partner_fee_tax : Fraction
+            The fraction of partner fees which need to be paid to the CoW DAO.
+
+    Notes
+    -----
+    All data frames are set to have data type `object`. Otherwise, implicit conversion to int64 can
+    lead to overflows.
+    """
+
+    partner_fees_dict: defaultdict[str, int] = defaultdict(int)
+    for _, row in partner_fee_lists.iterrows():
+        if row["partner_list"] is None:
+            continue
+
+        # We assume the two lists used below, i.e.,
+        # partner_list and partner_fee_eth,
+        # are "aligned".
+
+        for partner, partner_fee in zip(row["partner_list"], row["partner_fee_eth"]):
+            partner_fees_dict[partner] += int(partner_fee)
+
+    partner_fees_df = pd.DataFrame(
+        list(partner_fees_dict.items()),
+        columns=["partner", "partner_fee_eth"],
+    )
+
+    partner_fees_df["partner_fee_tax"] = np.where(
+        partner_fees_df["partner"] == config.reduced_cut_address,
+        config.partner_fee_reduced_cut,
+        config.partner_fee_cut,
+    )
+
+    # change all types to object to use native python types
+    partner_fees_df = partner_fees_df.astype(object)
+
+    return partner_fees_df
---
+++ src/fetch/solver_info.py
@@ -0,0 +1,121 @@
+"""Functionality for solver information."""
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+
+from src.config import AccountingConfig
+from src.logger import set_log
+
+log = set_log(__name__)
+
+REWARD_TARGETS_COLUMNS = ["solver", "reward_target", "pool_address", "solver_name"]
+SERVICE_FEES_COLUMNS = ["solver", "service_fee"]
+
+SOLVER_INFO_COLUMNS = [
+    "solver",
+    "solver_name",
+    "reward_target",
+    "buffer_accounting_target",
+    "service_fee",
+]
+
+
+def compute_solver_info(
+    reward_targets: DataFrame,
+    service_fees: DataFrame,
+    config: AccountingConfig,
+) -> DataFrame:
+    """Compute solver info.
+
+    Parameters
+    ----------
+    reward_targets : DataFrame
+        Data on reward targets and bonding pools.
+        The columns have to contain REWARD_TARGETS_COLUMNS:
+        - solver : str
+            "0x"-prefixed hex representation of the submission address of a solver.
+        - reward_target : str
+            "0x"-prefixed hex representation of the reward target of a solver. All rewards are sent
+            to this address.
+        - pool_address: str
+            "0x"-prefixed hex representation of address of a solvers bonding pool.
+        - solver_name: str
+            Name of a solver.
+
+    service_fees : DataFrame
+        Service fee data.
+        The columns have to contain SERVICE_FEES_COLUMNS:
+        - solver : str
+            "0x"-prefixed hex representation of the submission address of a solver.
+        - service_fee : bool
+            True if a solver needs to pay a service fee to the CoW DAO. Otherwise, it is False.
+
+    config : AccountingConfig
+        Accounting configuration.
+
+    Returns
+    -------
+    solver_info : DataFrame
+        Data frame containing required information per solver.
+        The columns are SOLVER_INFO_COLUMNS:
+        - solver : str
+            "0x"-prefixed hex representation of the submission address of a solver.
+        - solver_name : str
+            Name of a solver.
+        - reward_target : str
+            "0x"-prefixed hex representation of the reward target of a solver. All
+            rewards are sent to this address.
+        - buffer_accounting_target : str
+            "0x"-prefixed hex representation of the buffer accounting target address of a solver.
+            Results of the buffer accounting are sent to this address. It is equal to `solver` or
+            `reward_target`.
+        - service_fee : Fraction
+            The fraction of rewards which need to be paid to the CoW DAO.
+
+    Raises
+    ------
+    AssertionError
+        If input dataframes do not contain required columns or if the result does not have correct
+        columns.
+
+    Notes
+    -----
+    All data frames are set to have data type `object`. Otherwise, implicit conversion to int64 can
+    lead to overflows.
+    """
+
+    # validate reward targets and service fees columns
+    assert set(REWARD_TARGETS_COLUMNS).issubset(set(reward_targets.columns))
+    assert set(SERVICE_FEES_COLUMNS).issubset(set(service_fees.columns))
+
+    solver_info = reward_targets[REWARD_TARGETS_COLUMNS].merge(
+        service_fees[SERVICE_FEES_COLUMNS], how="outer", on="solver"
+    )
+
+    solver_info["buffer_accounting_target"] = np.where(
+        solver_info["pool_address"] != config.reward_config.cow_bonding_pool.address,
+        solver_info["solver"],
+        solver_info["reward_target"],
+    )
+    solver_info = solver_info.drop("pool_address", axis=1)
+
+    solver_info["service_fee"] = [
+        (
+            service_fees_flag * config.reward_config.service_fee_factor
+            if not pd.isna(service_fees_flag)
+            else 0 * config.reward_config.service_fee_factor
+        )
+        for service_fees_flag in solver_info["service_fee"]
+    ]
+
+    if not solver_info["solver"].is_unique:
+        duplicate_solvers = solver_info[solver_info["solver"].duplicated(keep=False)]
+        log.warning(f"Duplicate solvers: {duplicate_solvers}. Choosing first entry.")
+        solver_info = solver_info.drop_duplicates(subset=["solver"])
+
+    solver_info = solver_info.astype(object)
+
+    assert set(solver_info.columns) == set(SOLVER_INFO_COLUMNS)
+
+    return solver_info
