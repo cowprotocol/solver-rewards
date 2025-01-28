430b7a51361abf75e99ad12df33a7b2d1f6bc0b8
harisang <harisangelidakis@gmail.com>
2025-01-29 01:00:45 +0200

    fix syntax of new query

--- queries/orderbook/auction_prices_corrections.sql
+++ queries/orderbook/auction_prices_corrections.sql
@@ -5,4 +5,4 @@ auction_price_corrections (blockchain, environment, auction_id, price) as ( --no
         ('ethereum', 'prod', '\x5c47902c8c80779cb99235e42c354e53f38c3b0d'::bytea, 101902651::numeric(78, 0))
     ) as temp(blockchain, environment, auction_id, price)
     where blockchain = '{{blockchain}}' and environment = '{{environment}}'
-)
+),
