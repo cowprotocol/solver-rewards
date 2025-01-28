734cfb6ff6c805335eea8e4c1580b32254c0863e
harisang <harisangelidakis@gmail.com>
2025-01-29 01:12:27 +0200

    fix new query

--- queries/orderbook/auction_prices_corrections.sql
+++ queries/orderbook/auction_prices_corrections.sql
@@ -1,8 +1,8 @@
-auction_prices_corrections (blockchain, environment, auction_id, price) as ( --noqa: PRS
+auction_prices_corrections (blockchain, environment, auction_id, token, price) as ( --noqa: PRS
     select *
     from (
         values 
-        ('ethereum', 'prod', '\x5c47902c8c80779cb99235e42c354e53f38c3b0d'::bytea, 101902651::numeric(78, 0))
-    ) as temp(blockchain, environment, auction_id, price)
+        ('ethereum', 'prod', 10105124::bigint, '\x5c47902c8c80779cb99235e42c354e53f38c3b0d'::bytea, 101902651::numeric(78, 0))
+    ) as temp(blockchain, environment, auction_id, token, price)
     where blockchain = '{{blockchain}}' and environment = '{{environment}}'
 ),
