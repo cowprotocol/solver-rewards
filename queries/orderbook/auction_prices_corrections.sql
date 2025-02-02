d624a434be54605bb702a064f2cff8bfcd9018ed
harisang <harisangelidakis@gmail.com>
2025-02-03 01:09:45 +0200

    fix typos

--- queries/orderbook/auction_prices_corrections.sql
+++ queries/orderbook/auction_prices_corrections.sql
@@ -75,10 +75,10 @@ auction_prices_corrections (blockchain, environment, auction_id, token, price) a
         ('ethereum', 'prod', 10104715::bigint, '\x1a88df1cfe15af22b3c4c783d4e6f7f9e0c1885d'::bytea, 318978711000518::numeric(78, 0)),
 
         -- 0xd6bd149df0d16cc26b1e2581e25340bf6aa31bf218cbffd4cbb5e55e9d1dfae2, network fee issue
-        ('ethereum', 'prod', 10104624::bigint, '\x4eca7761a516f8300711cbf920c0b85555261993'::bytea, 23670618::numeric(78, 0))
+        ('ethereum', 'prod', 10104624::bigint, '\x4eca7761a516f8300711cbf920c0b85555261993'::bytea, 23670618::numeric(78, 0)),
 
         -- correction only relevant for the tests in the test_batch_rewards.py file
-        ('ethereum', 'prod', 53::bigint, '\x02'::bytea, 500000000000000::numeric(78, 0)),
+        ('ethereum', 'prod', 53::bigint, '\x02'::bytea, 500000000000000::numeric(78, 0))
 )
 
     ) as temp(blockchain, environment, auction_id, token, price)
