8a40a51ff0b66385ec2ffa08ffd528b1d5a42074
Pierce Roberts <pierceroberts95@gmail.com>
2025-03-15 17:11:43 -0600

    add init test for find_block_with_timestamp

--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -1,7 +1,10 @@
 import pytest
 from pytest_mock import MockerFixture
 import datetime
-from src.data_sync.common import partition_time_range, compute_block_range
+from src.data_sync.common import (
+    partition_time_range, 
+    compute_block_range, 
+    find_block_with_timestamp)
 
 from web3 import Web3
 
@@ -38,7 +41,7 @@ def test_partition_time_range(mocker: MockerFixture):
 
     assert len(result) == 3
 
-def test_compute_block_range(mocker: MockerFixture,times, mock_node):
+def test_compute_block_range(mocker: MockerFixture, times, mock_node):
     mock_find_block = mocker.patch("src.data_sync.common.find_block_with_timestamp", side_effect=[200, 400])
     mock_get_block = mocker.patch.object(mock_node.eth, "get_block", return_value={
         "number": 400,
@@ -52,3 +55,12 @@ def test_compute_block_range(mocker: MockerFixture,times, mock_node):
     mock_get_block.assert_called_once()
     assert results.block_from == 200
     assert results.block_to == 399
+
+def test_find_block_with_timestamp(mocker: MockerFixture, mock_node, times):
+    end_timestamp = times["end_time"].timestamp()
+    mock_get_block = mocker.patch.object(mock_node.eth, "get_block", return_value={
+        "number": 400,
+        "timestamp": end_timestamp})
+    results = find_block_with_timestamp(node=mock_node, time_stamp=end_timestamp)
+    assert results == 400
+    assert mock_get_block.call_count == 3
