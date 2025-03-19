9837de216eb67fabf2a90f902031aa961a6e87ae
Pierce Roberts <pierceroberts95@gmail.com>
2025-03-18 21:36:25 -0600

    run black

--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -2,20 +2,20 @@ import pytest
 from pytest_mock import MockerFixture
 import datetime
 from src.data_sync.common import (
-    partition_time_range, 
-    compute_block_range, 
-    find_block_with_timestamp)
+    partition_time_range,
+    compute_block_range,
+    find_block_with_timestamp,
+)
 
 from web3 import Web3
+
 
 @pytest.fixture(scope="session")
 def times():
     start_time = datetime.datetime.now(tz=datetime.timezone.utc)
     end_time = start_time + datetime.timedelta(seconds=10)
-    return{
-        "start_time": start_time,
-        "end_time": end_time
-    }
+    return {"start_time": start_time, "end_time": end_time}
+
 
 @pytest.fixture(scope="function")
 def mock_node():
@@ -23,48 +23,65 @@ def mock_node():
 
 
 def test_partition_time_range(times):
-
     # test that if they are equal or negative the function raises an error
     with pytest.raises(AssertionError):
-        partition_time_range(start_time=times["start_time"], end_time=times["start_time"])
-    
+        partition_time_range(
+            start_time=times["start_time"], end_time=times["start_time"]
+        )
+
     # test timezones don't match
     with pytest.raises(AssertionError):
-        partition_time_range(start_time=datetime.datetime.now(), end_time=times["end_time"])
-    
+        partition_time_range(
+            start_time=datetime.datetime.now(), end_time=times["end_time"]
+        )
+
     with pytest.raises(AssertionError):
-        partition_time_range(start_time=times["start_time"], end_time=times["end_time"] + datetime.timedelta(seconds=-10))
-    
+        partition_time_range(
+            start_time=times["start_time"],
+            end_time=times["end_time"] + datetime.timedelta(seconds=-10),
+        )
+
     # end time is not greater than a month
-    result = partition_time_range(start_time=times["start_time"], end_time=times["end_time"])
+    result = partition_time_range(
+        start_time=times["start_time"], end_time=times["end_time"]
+    )
     assert result == [(times["start_time"], times["end_time"])]
 
     # end time is greater than a month
-    result = partition_time_range(start_time=times["start_time"], end_time=times["end_time"] + datetime.timedelta(days=60))
+    result = partition_time_range(
+        start_time=times["start_time"],
+        end_time=times["end_time"] + datetime.timedelta(days=60),
+    )
 
     assert len(result) == 3
 
+
 def test_compute_block_range(mocker: MockerFixture, times, mock_node):
-    mock_find_block = mocker.patch("src.data_sync.common.find_block_with_timestamp", side_effect=[200, 400])
-    mock_get_block = mocker.patch.object(mock_node.eth, "get_block", return_value={
-        "number": 400,
-        "timestamp": times["end_time"].timestamp()})
+    mock_find_block = mocker.patch(
+        "src.data_sync.common.find_block_with_timestamp", side_effect=[200, 400]
+    )
+    mock_get_block = mocker.patch.object(
+        mock_node.eth,
+        "get_block",
+        return_value={"number": 400, "timestamp": times["end_time"].timestamp()},
+    )
     results = compute_block_range(
-        start_time=times["start_time"], 
-        end_time=times["end_time"], 
-        node=mock_node)
-    
+        start_time=times["start_time"], end_time=times["end_time"], node=mock_node
+    )
+
     assert mock_find_block.call_count == 2
     mock_get_block.assert_called_once()
     assert results.block_from == 200
     assert results.block_to == 399
 
+
 def test_find_block_with_timestamp(mocker: MockerFixture, mock_node, times):
     end_timestamp = times["end_time"].timestamp()
-    mock_get_block = mocker.patch.object(mock_node.eth, "get_block", return_value={
-        "number": 400,
-        "timestamp": end_timestamp})
+    mock_get_block = mocker.patch.object(
+        mock_node.eth,
+        "get_block",
+        return_value={"number": 400, "timestamp": end_timestamp},
+    )
     results = find_block_with_timestamp(node=mock_node, time_stamp=end_timestamp)
     assert results == 400
     assert mock_get_block.call_count == 3
-
