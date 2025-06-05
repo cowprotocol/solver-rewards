dbca8dc097d1a15a5078f288eb9674139fe3e59d
Felix Henneke <felix.henneke@protonmail.com>
2025-06-05 16:56:08 +0200

    clean up data fetching

--- src/pg_client.py
+++ src/pg_client.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-import os
-
 import pandas as pd
 from pandas import DataFrame, Series, read_sql_query
 from sqlalchemy import create_engine
@@ -170,24 +168,47 @@ class MultiInstanceDBFetcher:
 
         return results_df
 
-    def get_data_per_solver(
-        self, accounting_period: AccountingPeriod, config: AccountingConfig
+    def get_analytics_db_table_prod_and_barn(
+        self,
+        table_name: str,
+        accounting_period: AccountingPeriod,
+        config: AccountingConfig,
     ) -> DataFrame:
+        # pylint: disable=too-many-locals
         """
+        Constructs and executes a query on an analytics database for specific environments
+        and consolidates the results into a single DataFrame. The function fetches data
+        from multiple database environments (`prod`, `staging`) for a given accounting
+        period and specified table, combining rows from each environment into a
+        unified Pandas DataFrame.
+
+        Parameters
+        ----------
+        table_name : str
+            The name of the database table to query.
+        accounting_period : AccountingPeriod
+            The period for which the data should be fetched. The AccountingPeriod
+            object must have `start` and `end` attributes formatted as datetime
+            objects.
+        config : AccountingConfig
+            Configuration object containing database connection details such as
+            analytics_db_url, network_db_name, and schema.
 
         Returns
         -------
         DataFrame
+            A Pandas DataFrame containing the concatenated query results from the
+            `prod` and `staging` environments.
         """
-        db_url = os.environ[
-            "ANALYTICS_DB_URL"
-        ]  # this is not compatible with the current format
-        network = "mainnet"  # this should not be hardcoded
-        schema = "felix"  # this should not be hardcoded
+        db_url = (
+            config.orderbook_config.analytics_db_url
+        )  # this is not compatible with the current format
+        network = config.orderbook_config.network_db_name
+        schema = config.orderbook_config.schema
         start_time_string = accounting_period.start.strftime("%Y-%m-%d %H:%M:%S")
         end_time_string = accounting_period.end.strftime("%Y-%m-%d %H:%M:%S")
         accounting_period_string = f"{start_time_string} - {end_time_string}"
-        query = f"""SELECT * FROM {schema}.fct_data_per_solver_and_accounting_period
+        query = f"""SELECT * FROM {schema}.{table_name}
         where accounting_period = '{accounting_period_string}'"""
         result_list = []
         # for environment in ["prod"]:
@@ -211,9 +232,40 @@ class MultiInstanceDBFetcher:
 
         results = pd.concat(result_list).reset_index(drop=True)
 
+        return results
+
+    def get_data_per_solver(
+        self, accounting_period: AccountingPeriod, config: AccountingConfig
+    ) -> DataFrame:
+        """Fetches and processes solver-related data for a specific accounting period.
+
+        This function retrieves data for a given accounting period and configuration by
+        querying the prod and barn database. The results are
+        then processed to convert specific columns (e.g., solver, pool_address, and
+        reward_target) from bytearray format to hexadecimal format for standardized
+        representation.
+
+        Parameters
+        ----------
+        accounting_period : AccountingPeriod
+            The accounting period object used to filter data for the required time range.
+        config : AccountingConfig
+            Configuration detailing settings and parameters for the data retrieval and
+            processing operations.
+
+        Returns
+        -------
+        DataFrame
+            A DataFrame containing processed data for each solver, with specific fields
+            converted to hexadecimal format for appropriate representation.
+        """
+        results = self.get_analytics_db_table_prod_and_barn(
+            "fct_data_per_solver_and_accounting_period", accounting_period, config
+        )
+
         hex_columns = ["solver", "pool_address", "reward_target"]
         for column in hex_columns:
-            results[column] = results[column].apply(lambda x: "0x" + x.hex())
+            results[column] = results[column].apply(bytearray2hex)
 
         return results
 
@@ -221,49 +273,58 @@ class MultiInstanceDBFetcher:
         self, accounting_period: AccountingPeriod, config: AccountingConfig
     ) -> DataFrame:
         """
+        Fetches and processes partner and protocol fees data from an analytics database table.
+
+        This function retrieves data related to partner and protocol fees for a specified
+        accounting period and configuration. The data is fetched from the analytics database
+        production and barn tables. Additionally, the function processes specific columns to
+        convert byte data into hexadecimal format for further usage.
+
+        Parameters
+        ----------
+        accounting_period : AccountingPeriod
+            The accounting period for which the partner and protocol fees data is to be fetched.
+        config : AccountingConfig
+            Configuration settings specifying the analytics database and environment.
 
         Returns
         -------
         DataFrame
+            A Pandas DataFrame containing the fetched and processed partner and protocol
+            fees data. Specific columns in this DataFrame have been converted into
+            hexadecimal format.
         """
-        db_url = os.environ[
-            "ANALYTICS_DB_URL"
-        ]  # this is not compatible with the current format
-        network = "mainnet"  # this should not be hardcoded
-        schema = "felix"  # this should not be hardcoded
-        start_time_string = accounting_period.start.strftime("%Y-%m-%d %H:%M:%S")
-        end_time_string = accounting_period.end.strftime("%Y-%m-%d %H:%M:%S")
-        accounting_period_string = f"{start_time_string} - {end_time_string}"
-        query = f"""SELECT * FROM {schema}.fct_partner_and_protocol_fees
-where accounting_period = '{accounting_period_string}'"""
-        result_list = []
-        for environment in ["prod", "staging"]:
-            pg_engine = create_engine(
-                f"postgresql+psycopg2://{db_url}/{environment}_{network}",
-                pool_pre_ping=True,
-                connect_args={
-                    "keepalives": 1,
-                    "keepalives_idle": 30,
-                    "keepalives_interval": 10,
-                    "keepalives_count": 5,
-                },
-            )
-            with pg_engine.connect() as conn:
-                result = read_sql_query(
-                    query,
-                    conn,
-                )
-                result_list.append(result)
-
-        results = pd.concat(result_list).reset_index(drop=True)
+        results = self.get_analytics_db_table_prod_and_barn(
+            "fct_partner_and_protocol_fees", accounting_period, config
+        )
 
         hex_columns = ["partner_fee_recipient"]
         for column in hex_columns:
-            results[column] = results[column].apply(
-                lambda x: "0x" + x.hex() if x else x
-            )
+            results[column] = results[column].apply(bytearray2hex)
 
         return results
+
+
+def bytearray2hex(address: bytearray) -> str | None:
+    """Converts a bytearray into its hexadecimal string representation.
+
+    This function takes a bytearray input and transforms it into a
+    string representation of its hexadecimal value, prefixed with "0x".
+    If the input bytearray is empty, the original value is returned.
+
+    Parameters
+    ----------
+    address : bytearray
+        The bytearray to be converted into a hexadecimal string.
+
+    Returns
+    -------
+    str
+        A string representing the hexadecimal equivalent of the input
+        bytearray, prefixed with "0x". If the bytearray is empty, the
+        original `address` is returned.
+    """
+    return "0x" + address.hex() if address else None
 
 
 def pg_hex2bytea(hex_address: str) -> str:
