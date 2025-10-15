b8db8b8b56bfc74d4964869af471a8a23087f909
harisang <harisangelidakis@gmail.com>
2025-10-15 16:17:44 +0300

    black fix

--- src/fetch/transfer_file.py
+++ src/fetch/transfer_file.py
@@ -169,7 +169,7 @@ def auto_propose(
         slack_channel = config.io_config.slack_channel
         assert slack_channel is not None
 
-        nonce_cow: int | None = post_multisend(
+        nonce_cow = post_multisend(
             safe_address=config.payment_config.payment_safe_address_cow,
             transactions=transactions_cow,
             network=EthereumNetwork.MAINNET,
@@ -178,7 +178,7 @@ def auto_propose(
             nonce_modifier=config.payment_config.nonce_modifier,
         )
 
-        nonce_native: int | None = post_multisend(
+        nonce_native = post_multisend(
             safe_address=config.payment_config.payment_safe_address_native,
             transactions=transactions_native,
             network=config.payment_config.network,
@@ -187,11 +187,11 @@ def auto_propose(
             nonce_modifier=(
                 len(Network)
                 if config.payment_config.network == EthereumNetwork.MAINNET
-                else (1 if nonce_native is not None else 0)
+                else 0
             ),
         )
 
-        nonce_overdrafts: int | None = post_multisend(
+        nonce_overdrafts = post_multisend(
             safe_address=config.payment_config.payment_safe_address_native,
             transactions=ovedrafts_txs,
             network=config.payment_config.network,
