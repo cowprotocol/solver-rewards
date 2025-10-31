e5385b3c27f1e94c7f8bbb2f284bfd483fc32684
Bram van den Berg <bram@cow.fi>
2025-10-31 09:16:55 +0900

    Add contract address for Lens (#601)
    This PR updates the contract address for the overdrafts contract on Lens
    network to adapt to the changes from [this
    pr](https://github.com/cowprotocol/overdrafts-manager/pull/3).
    
    ---------
    
    Co-authored-by: Haris Angelidakis <64154020+harisang@users.noreply.github.com>

--- src/models/overdraft.py
+++ src/models/overdraft.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import os
 from dataclasses import dataclass
 
 from dune_client.types import Address
@@ -9,6 +10,7 @@ from safe_eth.safe.multi_send import MultiSendOperation, MultiSendTx
 from web3 import Web3
 from src.abis.load import overdraftsmanager
 from src.models.accounting_period import AccountingPeriod
+from src.config import OverdraftConfig, Network
 
 OVERDRAFTS_CONTRACT = overdraftsmanager()
 
@@ -32,9 +34,12 @@ class Overdraft:
 
     def as_multisend_tx(self) -> MultiSendTx:
         """Converts Overdraft into encoded MultiSendTx bytes"""
+        network = Network(os.getenv("NETWORK"))
+        config = OverdraftConfig.from_network(network)
+        contract_address = Web3.to_checksum_address(config.contract_address.address)
         return MultiSendTx(
             operation=MultiSendOperation.CALL,
-            to=Web3.to_checksum_address("0x8Fd67Ea651329fD142D7Cfd8e90406F133F26E8a"),
+            to=contract_address,
             value=0,
             data=OVERDRAFTS_CONTRACT.encode_abi(
                 abi_element_identifier="addOverdraft",
