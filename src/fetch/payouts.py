40bf3a8e4febe91f99da43a97dae90ce56158432
harisang <harisangelidakis@gmail.com>
2026-06-04 02:54:32 +0300

    black fix

--- src/fetch/payouts.py
+++ src/fetch/payouts.py
@@ -367,10 +367,8 @@ def prepare_payouts(  # pylint: disable=too-many-locals
         partner = row["partner"]
         partner_fee = int(row["partner_fee_eth"] * (1 - row["partner_fee_tax"]))
         assert partner_fee >= 0, f"Can't construct negative transfer of {partner_fee}"
-        if (
-            partner_fee > 0
-            and Address(partner)
-            != Address("0x81BA8A2b895D30280bca199C2Ff75f3F058d4C6c")
+        if partner_fee > 0 and Address(partner) != Address(
+            "0x81BA8A2b895D30280bca199C2Ff75f3F058d4C6c"
         ):
             transfers.append(
                 Transfer(
